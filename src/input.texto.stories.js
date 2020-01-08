6cfd761d6b2dd3e2ac5279e9573fd974b8e82e70
MitziYolotzin <mitziulter@gmail.com>
2020-01-08 15:48:56 -0600

    text area elemento y vista, ajuste inicial es stories

--- src/input.texto.stories.js
+++ src/input.texto.stories.js
@@ -1,4 +1,5 @@
 import InputTextoView from './stories/formularios/InputTextoView.svelte';
+import TextAreaVista from './stories/formularios/TextAreaVista.svelte';
 
 export default { title: 'Formularios' }
 
@@ -15,4 +16,11 @@ export const inputTexto = () => ({
     props: {
         textoValidar: 'Hola mundo',
     },
+})
+// text area
+export const textArea = () => ({
+    Component:TextAreaVista,
+    props: {
+        texto: 'Hola mundo'
+    }
 })
