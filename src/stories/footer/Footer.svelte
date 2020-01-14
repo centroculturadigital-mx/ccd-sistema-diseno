c5a342be4266aec70cbfe929b3ee488fc7de5f3b
ledautopica <vane.multimedia@gmail.com>
2020-01-13 18:20:50 -0600

    subí footer

---
+++ src/stories/footer/Footer.svelte
@@ -0,0 +1,20 @@
+<script>
+  import LogoVista from "../logo/LogoVista.svelte";
+  import BotonMenuMovilVista from "../botones/BotonMenuMovil/BotonMenuMovilVista.svelte";
+  import MenuEscritorioVista from "../menu/MenuEscritorio/MenuEscritorioVista.svelte";
+  import MenuMovilVista from "../menu/MenuMovil/MenuMovilVista.svelte";
+
+  export let logoTexto;
+  export let logoImagenUrl;
+  export let objetosMenu;
+  export let altura;
+  export let colorFondo;
+  export let color;
+  export let sombra;
+  export let fixed;
+  export let estadoMenu;
+
+  //  Responsivo
+  let responsivo;
+  let breakpoint = 720;
+</script>
