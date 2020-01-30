8580546118734ba90c1163a30e7bc874d0e8cc5e
josecaos <jazzvoon@gmail.com>
2020-01-30 15:18:45 -0600

    Fix a elementos exportados

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ export { default as MenuEscritorio } from "./elementos/menu/MenuEscritorio/MenuE
 export { default as MenuMovil } from "./elementos/menu/MenuMovil/MenuMovil.svelte";
 export { default as Titulo } from "./elementos/texto/Titulo/Titulo.svelte";
 export { default as Parrafo } from "./elementos/texto/Parrafo/Parrafo.svelte";
-export { default as Input } from "./elementos/input/Input/Input.svelte";
-export { default as InputEmail } from "./elementos/input/InputEmail/InputEmail.svelte";
-export { default as InputSelect } from "./elementos/input/InputSelect/InputSelect.svelte";
-export { default as TextArea } from "./elementos/input/TextArea/TextArea.svelte";
+export { default as Input } from "./elementos/inputs/Input/Input.svelte";
+export { default as InputEmail } from "./elementos/inputs/InputEmail/InputEmail.svelte";
+export { default as InputSelect } from "./elementos/inputs/InputSelect/InputSelect.svelte";
+export { default as TextArea } from "./elementos/inputs/TextArea/TextArea.svelte";
 
 //  Componentes
 export { default as Header } from "./componentes/Header/Header.svelte";
