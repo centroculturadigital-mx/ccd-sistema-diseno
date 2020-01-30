8764e7d3b74edc99748cfe2f0dd54546b22ddb0c
josecaos <jazzvoon@gmail.com>
2020-01-30 15:00:47 -0600

    Exporta todos los elementos

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
-export { default as Header } from "./componentes/Header/Header.svelte";
+// Elementos
 export { default as Boton } from "./elementos/botones/Boton/Boton.svelte";
+export { default as BotonIcono } from "./elementos/botones/Boton/BotonIcono.svelte";
+export { default as Imagen } from "./elementos/media/Imagen/Imagen.svelte";
+export { default as Video } from "./elementos/media/Video/Video.svelte";
+export { default as MenuEscritorio } from "./elementos/menu/MenuEscritorio/MenuEscritorio.svelte";
+export { default as MenuMovil } from "./elementos/menu/MenuMovil/MenuMovil.svelte";
+export { default as Titulo } from "./elementos/texto/Titulo/Titulo.svelte";
+export { default as Parrafo } from "./elementos/texto/Parrafo/Parrafo.svelte";
+export { default as Input } from "./elementos/input/Input/Input.svelte";
+export { default as InputEmail } from "./elementos/input/InputEmail/InputEmail.svelte";
+export { default as InputSelect } from "./elementos/input/InputSelect/InputSelect.svelte";
+export { default as TextArea } from "./elementos/input/TextArea/TextArea.svelte";
+
+//  Componentes
+export { default as Header } from "./componentes/Header/Header.svelte";
+
