fc9f347a8fff6f8197998245fbfe9afff0ae094d
furenku <furenku@gmail.com>
2020-03-01 15:56:45 -0600

    merge dev->master

---
+++ src/elementos/botones/BotonMenu/BotonMenu.svelte
@@ -0,0 +1,33 @@
+<script>
+
+  import MenuIcon from "svelte-feather-icons/src/icons/MenuIcon.svelte";
+  import XIcon from "svelte-feather-icons/src/icons/XIcon.svelte";
+  
+  export let estado=false;
+
+</script>
+
+<style>
+  button {
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    width: 3rem;
+    margin: 0;
+  }
+  :global(.feather) {
+    stroke: black;
+    
+  }
+  :global(.feather:hover) {
+    stroke: red;
+  }
+</style>
+
+<button on:click>
+  {#if !! estado}
+    <XIcon />
+  {:else}
+    <MenuIcon />
+  {/if}
+</button>
