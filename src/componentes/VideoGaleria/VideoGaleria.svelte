b96ba0d5579ead3ee387d095fed2eb4293068de4
josecaos <jazzvoon@gmail.com>
2020-03-24 15:22:45 -0600

    Agrega iconos

--- src/componentes/VideoGaleria/VideoGaleria.svelte
+++ src/componentes/VideoGaleria/VideoGaleria.svelte
@@ -4,13 +4,15 @@
   import VideoTarjeta from "./VideoTarjeta/VideoTarjeta.svelte";
   import VideosLista from "./VideosLista/VideosLista.svelte";
 
+  import iconoCierra from "../../../public/cerrar_blanco.svg";
+
   export let videos;
-  let estado = false;
+  let estado;
 
-  const alternarEstado = () => { estado = ! estado }
-
-  console.log(estado);
-  
+  const alternarEstado = () => {
+    estado = !estado;
+    console.log(estado);
+  };
 
 </script>
 
@@ -27,19 +29,22 @@
     {#if estado == true}
       <header>
         <a href="#" on:click|preventDefault={alternarEstado}>
-          <BotonIcono iconoBotonEstadoUnoUrl="cerrar_blanco.svg" />
+          <BotonIcono iconoBotonEstadoUnoUrl={iconoCierra}/>
         </a>
       </header>
-      <VideoReproductor url={videos[0].url}/>
+      <VideoReproductor url={videos[0].url} />
     {:else}
-        <VideoTarjeta titulo={videos[0].titulo} imagen={videos[0].imagen} abrir={alternarEstado}/>
+      <VideoTarjeta
+        titulo={videos[0].titulo}
+        imagen={videos[0].imagen}
+        abrir={alternarEstado} />
     {/if}
 
   </section>
 
   <section class="contenedorLista">
 
-    <VideosLista {videos}/>
+    <VideosLista {videos} />
 
   </section>
 
--- src/componentes/VideoGaleria/VideoTarjeta/VideoTarjeta.svelte
+++ src/componentes/VideoGaleria/VideoTarjeta/VideoTarjeta.svelte
@@ -1,36 +1,32 @@
 <script>
-import Imagen from '../../../elementos/media/Imagen/Imagen.svelte';
-import Titulo from '../../../elementos/texto/Titulo/Titulo.svelte';
-import BotonIcono from '../../../elementos/botones/BotonIcono/BotonIcono.svelte';
+  import Imagen from "../../../elementos/media/Imagen/Imagen.svelte";
+  import Titulo from "../../../elementos/texto/Titulo/Titulo.svelte";
+  import BotonIcono from "../../../elementos/botones/BotonIcono/BotonIcono.svelte";
 
+  import iconoPlay from "../../../../public/play.svg";
 
-export let titulo;
-export let imagen;
-export let abrir;
+  export let titulo;
+  export let imagen;
+  export let abrir;
 
-
-let alturaImagen = '400px';
-
+  let alturaImagen = "400px";
 </script>
 
 <article class="videoTarjeta">
 
-    <div class="imagen">
-        <Imagen 
-        imagenUrl={imagen}
-        altTexto={titulo}
-        alineacion={"center"}
-        altura={alturaImagen}
-        />
+  <div class="imagen">
+    <Imagen
+      imagenUrl={imagen}
+      altTexto={titulo}
+      alineacion={'center'}
+      altura={alturaImagen} />
 
-        <a href="#" on:click|preventDefault={abrir}>
-        <BotonIcono iconoBotonEstadoUnoUrl="play.svg"/>
-        </a>
+    <a href="#" on:click|preventDefault={abrir}>
+      <BotonIcono iconoBotonEstadoUnoUrl={iconoPlay} />
+    </a>
 
-    </div>
+  </div>
 
-
-<Titulo nivel={1} texto={titulo}/>
-
+  <Titulo nivel={1} texto={titulo} />
 
 </article>
