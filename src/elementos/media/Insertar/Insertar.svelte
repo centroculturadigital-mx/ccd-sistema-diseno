6e93cd6e8519cf8017cf4a4233d8ca2489bb50f4
josecaos <jazzvoon@gmail.com>
2020-03-23 23:57:57 -0600

    Nuevo Elemento <Insertar/> + prueba de Elemento en VideoGaleria

---
+++ src/elementos/media/Insertar/Insertar.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let url;
+  export let alto;
+  export let ancho;
+</script>
+
+<iframe
+  width={ancho}
+  height={alto}
+  src={url}
+  frameborder="0"
+  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+  allowfullscreen />
--- src/index.js
+++ src/index.js
@@ -1,25 +1,49 @@
 // Elementos
-export { default as Boton } from "./elementos/botones/Boton/Boton.svelte";
-export { default as BotonIcono } from "./elementos/botones/BotonIcono/BotonIcono.svelte";
-export { default as Imagen } from "./elementos/media/Imagen/Imagen.svelte";
-export { default as Video } from "./elementos/media/Video/Video.svelte";
-export { default as MenuEscritorio } from "./elementos/menu/MenuEscritorio/MenuEscritorio.svelte";
-export { default as MenuMovil } from "./elementos/menu/MenuMovil/MenuMovil.svelte";
-export { default as Titulo } from "./elementos/texto/Titulo/Titulo.svelte";
-export { default as Parrafo } from "./elementos/texto/Parrafo/Parrafo.svelte";
-export { default as Input } from "./elementos/inputs/Input/Input.svelte";
-export { default as InputEmail } from "./elementos/inputs/InputEmail/InputEmail.svelte";
-export { default as InputSelect } from "./elementos/inputs/InputSelect/InputSelect.svelte";
-export { default as TextArea } from "./elementos/inputs/TextArea/TextArea.svelte";
+export { default as Boton }
+from "./elementos/botones/Boton/Boton.svelte";
+export { default as BotonIcono }
+from "./elementos/botones/BotonIcono/BotonIcono.svelte";
+export { default as Imagen }
+from "./elementos/media/Imagen/Imagen.svelte";
+export { default as Video }
+from "./elementos/media/Video/Video.svelte";
+export { default as MenuEscritorio }
+from "./elementos/menu/MenuEscritorio/MenuEscritorio.svelte";
+export { default as MenuMovil }
+from "./elementos/menu/MenuMovil/MenuMovil.svelte";
+export { default as Titulo }
+from "./elementos/texto/Titulo/Titulo.svelte";
+export { default as Parrafo }
+from "./elementos/texto/Parrafo/Parrafo.svelte";
+export { default as Input }
+from "./elementos/inputs/Input/Input.svelte";
+export { default as InputEmail }
+from "./elementos/inputs/InputEmail/InputEmail.svelte";
+export { default as InputSelect }
+from "./elementos/inputs/InputSelect/InputSelect.svelte";
+export { default as TextArea }
+from "./elementos/inputs/TextArea/TextArea.svelte";
+export { default as Insertar }
+from "./elementos/media/Insertar/Insertar.svelte";
 
 //  Componentes
-export { default as Header } from "./componentes/Header/Header.svelte";
-export { default as Footer } from "./componentes/Footer/Footer.svelte";
-export { default as Label } from "./componentes/Label/Label.svelte";
-export { default as Logo } from "./componentes/Logo/Logo.svelte";
-export { default as Tarjeta } from "./componentes/Tarjetas/Tarjeta/Tarjeta.svelte";
-export { default as TarjetaHorizontal } from "./componentes/Tarjetas/TarjetaHorizontal/TarjetaHorizontal.svelte";
-export { default as TarjetaVertical } from "./componentes/Tarjetas/TarjetaVertical/TarjetaVertical.svelte";
-export { default as Contenido } from "./componentes/Contenido/Contenido.svelte";
-export { default as Mapa } from "./componentes/Mapa/Mapa.svelte";
-
+export { default as Header }
+from "./componentes/Header/Header.svelte";
+export { default as Footer }
+from "./componentes/Footer/Footer.svelte";
+export { default as Label }
+from "./componentes/Label/Label.svelte";
+export { default as Logo }
+from "./componentes/Logo/Logo.svelte";
+export { default as Tarjeta }
+from "./componentes/Tarjetas/Tarjeta/Tarjeta.svelte";
+export { default as TarjetaHorizontal }
+from "./componentes/Tarjetas/TarjetaHorizontal/TarjetaHorizontal.svelte";
+export { default as TarjetaVertical }
+from "./componentes/Tarjetas/TarjetaVertical/TarjetaVertical.svelte";
+export { default as Contenido }
+from "./componentes/Contenido/Contenido.svelte";
+export { default as Mapa }
+from "./componentes/Mapa/Mapa.svelte";
+export { default as VideoGaleria }
+from "./componentes/VideoGaleria/VideoGaleria.svelte";
