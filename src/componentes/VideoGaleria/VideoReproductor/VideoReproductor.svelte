c94bdf05ee4903c27531069c5c4597885cb151ce
josecaos <jazzvoon@gmail.com>
2020-03-25 01:37:38 -0600

    Estilos VideoGAleria Movil

--- src/componentes/VideoGaleria/VideoReproductor/VideoReproductor.svelte
+++ src/componentes/VideoGaleria/VideoReproductor/VideoReproductor.svelte
@@ -3,12 +3,22 @@ import Insertar from '../../../elementos/media/Insertar/Insertar.svelte';
 
 export let url;
 export let titulo;
-export let alto = "315";
-export let ancho  = "560";
+export let alto;
+export let ancho;
 </script>
 
+<style>
+.VideoReproductor {
+    height: 100%;
+    width: 100%;
+}
+.VideoReproductor :global(iframe) {
+    height: 100%;
+    width: 100%;
+}
+</style>
 
-<article>
+<article class="VideoReproductor">
 
 <Insertar {url} {alto} {ancho}/>
 
--- src/componentes/VideoGaleria/VideoVistaPrevia/VideoVistaPrevia.svelte
+++ src/componentes/VideoGaleria/VideoVistaPrevia/VideoVistaPrevia.svelte
@@ -1,27 +1,50 @@
 <script>
-  import Titulo from '../../../elementos/texto/Titulo/Titulo.svelte';
-  import Imagen from '../../../elementos/media/Imagen/Imagen.svelte';
+  import Titulo from "../../../elementos/texto/Titulo/Titulo.svelte";
+  import Imagen from "../../../elementos/media/Imagen/Imagen.svelte";
 
   export let imagen;
   export let titulo;
   export let indice;
   export let seleccionado;
+  export let activo;
+
 </script>
 
 <style>
-.VideoVistaPrevia {
+  .VideoVistaPrevia {
     cursor: pointer;
-}
-.VideoVistaPrevia:hover {
+    border-bottom: 1px solid #aaaaaa;
+    display: flex;
+    box-sizing: border-box;
+    padding: 1rem 0;
+  }
+  .VideoVistaPrevia:hover {
     opacity: 0.75;
-}
+  }
+  .VideoVistaPrevia :global(h4) {
+    width: 60%;
+    margin: 0;
+    display: flex;
+    align-items: center;
+    padding-left: 1rem;
+  }
+  .VideoVistaPrevia :global(h4 span) {
+    color: #666666;
+  }
+  .Imagen {
+    width: 40%;
+    flex-shrink: 0;
+  }
 </style>
 
-<article class="VideoVistaPrevia" on:click={seleccionado(indice)}>
-  <div class="imagen">
-    <Imagen imagenUrl={imagen} altTexto={titulo} altura={"4rem"}/>
+<article class="VideoVistaPrevia" on:click={seleccionado(indice)} style={activo?"lightgray":"transparent"}>
+
+  <div class="Imagen">
+
+    <Imagen imagenUrl={imagen} altTexto={titulo} altura={'5rem'} />
+
   </div>
 
-  <Titulo nivel={3} texto={titulo} />
+  <Titulo nivel={'4'} texto={titulo} />
 
 </article>
