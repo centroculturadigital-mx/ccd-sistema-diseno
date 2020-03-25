10ba57cfa3cdfa7e7df08b805002d4134ff47d60
josecaos <jazzvoon@gmail.com>
2020-03-24 18:15:58 -0600

    Ajustes a Seleccionar video

--- src/stories/componentes/VideoGaleria/video.galeria.stories.js
+++ src/stories/componentes/VideoGaleria/video.galeria.stories.js
@@ -2,33 +2,33 @@ import VideoGaleria from '../../../componentes/VideoGaleria/VideoGaleria.svelte'
 
 const videoGaleria = {
     videos: [{
-            url: "https://www.youtube.com/embed/rZUGniKYuZg",
-            titulo: "Video 1 Lorem Ipsum dolor sit amet Tiniebla",
+            url: "https://www.youtube.com/embed/U9OZFAAESRY",
+            titulo: "Video 1 Convocatoria Residencias Memorial",
             imagen: "https://www.agora-gallery.com/advice/wp-content/uploads/2015/10/image-placeholder-300x200.png"
         },
         {
-            url: "https://www.youtube.com/embed/rZUGniKYuZg",
-            titulo: "Video 2 Lorem Ipsum dolor sit amet Tiniebla",
+            url: "https://www.youtube.com/embed/OtKEAiWGi-Q",
+            titulo: "Video 2 CCD - Noche Hello World",
             imagen: "https://www.agora-gallery.com/advice/wp-content/uploads/2015/10/image-placeholder-300x200.png"
         },
         {
-            url: "https://www.youtube.com/embed/rZUGniKYuZg",
-            titulo: "Video 3 Lorem Ipsum dolor sit amet Tiniebla",
+            url: "https://www.youtube.com/embed/j5w0iT_-4cU",
+            titulo: "Video 3 Fiesta de la Nube Mala",
             imagen: "https://www.agora-gallery.com/advice/wp-content/uploads/2015/10/image-placeholder-300x200.png"
         },
         {
-            url: "https://www.youtube.com/embed/rZUGniKYuZg",
-            titulo: "Video 4 Lorem Ipsum dolor sit amet Tiniebla",
+            url: "https://www.youtube.com/embed/IF1CHAt08zg",
+            titulo: "Video 4 Global Game Jam 2020 GigaSede Los Pinos",
             imagen: "https://www.agora-gallery.com/advice/wp-content/uploads/2015/10/image-placeholder-300x200.png"
         },
         {
-            url: "https://www.youtube.com/embed/rZUGniKYuZg",
-            titulo: "Video 5 Lorem Ipsum dolor sit amet Tiniebla",
+            url: "https://www.youtube.com/embed/DfRfDhBgA5M",
+            titulo: "Video 5 ¡Va el golpe!",
             imagen: "https://www.agora-gallery.com/advice/wp-content/uploads/2015/10/image-placeholder-300x200.png"
         },
         {
-            url: "https://www.youtube.com/embed/rZUGniKYuZg",
-            titulo: "Video 6 Lorem Ipsum dolor sit amet Tiniebla",
+            url: "https://www.youtube.com/embed/TJ3DJPzP9Jw",
+            titulo: "Video 6 CCD - Invitación a Inauguración: Desobediencia Electrónica",
             imagen: "https://www.agora-gallery.com/advice/wp-content/uploads/2015/10/image-placeholder-300x200.png"
         }
     ]
