2305a7ecb2eaaa77f56b35a334f5820f5ad37d0d
furenku <furenku@gmail.com>
2020-03-25 19:14:48 -0600

    probar quitando botonicono de play en video

--- src/componentes/VideoGaleria/VideoTarjeta/VideoTarjeta.svelte
+++ src/componentes/VideoGaleria/VideoTarjeta/VideoTarjeta.svelte
@@ -2,7 +2,7 @@
   import Imagen from "../../../elementos/media/Imagen/Imagen.svelte";
   import BotonIcono from "../../../elementos/botones/BotonIcono/BotonIcono.svelte";
 
-  import iconoPlay from "../../../../public/botonPlay.png";
+  // import iconoPlay from "../../../../public/botonPlay.png";
 
   export let titulo;
   export let imagen;
@@ -50,7 +50,8 @@
       altura={alturaImagen} />
 
     <a href="#" on:click|preventDefault={abrir}>
-      <BotonIcono iconoBotonEstadoUnoUrl={iconoPlay} />
+      <!-- <BotonIcono iconoBotonEstadoUnoUrl={iconoPlay} /> -->
+      <img src={iconoPlay} alt="play"/>
     </a>
 
   </div>
