b8eb306f87e38acb9aaa81f3c032ce9927384cec
furenku <furenku@gmail.com>
2020-03-28 02:53:32 -0600

    probar chequeo videogaleria

--- src/componentes/VideoGaleria/VideoGaleria.svelte
+++ src/componentes/VideoGaleria/VideoGaleria.svelte
@@ -137,7 +137,7 @@
 
   <section class="ContenedorPrincipal">
 
-    {#if typeof videoActual == "object"}
+    {#if !! videoActual && typeof videoActual == "object"}
       
       <div class="ContenedorMedia">
         {#if estado }
