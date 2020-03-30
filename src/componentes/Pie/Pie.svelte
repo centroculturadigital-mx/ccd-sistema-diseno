2d27c98315bea9950abab7beb49a231a0aa361cc
josecaos <jazzvoon@gmail.com>
2020-03-29 20:17:55 -0600

    Nuevo modulo Pie + historias

---
+++ src/componentes/Pie/Pie.svelte
@@ -0,0 +1,56 @@
+<script>
+  import Logo from "../Logo/Logo.svelte";
+  import Logos from "../Logos/Logos.svelte";
+  import Parrafo from "../../elementos/texto/Parrafo/Parrafo.svelte";
+
+  export let fixed;
+  export let logos;
+  export let logoTexto;
+  export let logoImagenUrl;
+  export let altura;
+  export let copyright;
+  
+</script>
+
+<style>
+  footer {
+    background-color: lightgray;
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    box-sizing: border-box;
+    padding: 0.5rem 1rem;
+  }
+
+  .fixed {
+    position: fixed;
+    width: 100%;
+    bottom: 0;
+    left: 0;
+    z-index: 1000;
+  }
+  .logos {
+    width: auto;
+  }
+  .copyright :global(p span) {
+    font-size: 0.75rem;
+  } 
+</style>
+
+<footer style="height:{!!altura ? altura : ''};" class={!!fixed ? 'fixed' : ''}>
+  
+  <div class="logos">
+
+    {#if Array.isArray(logos)}
+      <Logos {logos} />
+    {:else if !!logoImagenUrl}
+      <Logo {logoTexto} {logoImagenUrl} logoAncho={'30%'} />
+    {/if}
+
+  </div>
+  <div class="copyright">
+    <Parrafo texto={copyright} />
+  </div>
+
+</footer>
---
+++ src/stories/componentes/Pie/pie.stories.js
@@ -0,0 +1,37 @@
+import Pie from '../../../componentes/Pie/Pie.svelte';
+import placeholder from '../../../../public/placeholder.jpg';
+
+export default { title: 'Componentes/Pie/Pie' }
+
+// export let fixed;
+// export let logos;
+// export let logoTexto;
+// export let logoImagenUrl;
+// export let altura;
+// export let copyright;
+
+
+console.log("!!!::: ", placeholder);
+
+export const PieDefault = () => ({
+    Component: Pie,
+    props: {
+        altura: '3rem',
+        logImagenUrl: "https://www.agora-gallery.com/advice/wp-content/uploads/2015/10/image-placeholder-300x200.png",
+    }
+})
+export const PieLogo = () => ({
+    Component: Pie,
+    props: {
+        altura: '3rem',
+        logImagenUrl: "https://www.agora-gallery.com/advice/wp-content/uploads/2015/10/image-placeholder-300x200.png",
+    }
+})
+export const PieCopyright = () => ({
+    Component: Pie,
+    props: {
+        altura: '3rem',
+        logImagenUrl: "https://www.agora-gallery.com/advice/wp-content/uploads/2015/10/image-placeholder-300x200.png",
+        copyright: "CCD Mexico | 2020"
+    }
+})
