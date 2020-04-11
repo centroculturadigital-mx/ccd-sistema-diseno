55db4e5c905d5fc7430c245a188000a0e0d7e41b
furenku <furenku@gmail.com>
2020-04-10 21:35:53 -0500

    Paginacion mejorada

---
+++ src/componentes/Paginacion/Paginacion.svelte
@@ -0,0 +1,80 @@
+<script>
+    
+    const revisarNumero = n => ((parseInt(n)===0)||(parseInt(n)>0))
+
+
+    export let elementos
+    export let elementosPagina=10
+    export let pagina=0
+    export let seleccionar
+
+    
+    $: elementosMostrar = revisarNumero(elementos) ?
+        elementos : Array.isArray(elementos) ?
+            elementos.length : null
+
+    
+    $: mostrar = revisarNumero(elementosMostrar) && typeof seleccionar == "function"
+    
+    $: paginasNum = Math.ceil( elementosMostrar / elementosPagina )
+    $: paginas = Array(3).fill(0).map((e,i)=>i)
+        
+    $: actual = paginaSeleccionada ? paginaSeleccionada : pagina ? pagina : 0
+
+    let paginaSeleccionada
+
+    const accion = (e,i) => {    
+
+        if( revisarNumero(i) && i < elementosMostrar ) {
+        
+            if(typeof seleccionar == "function") {
+                seleccionar(i)
+            }
+
+            paginaSeleccionada = i
+
+        }
+    }
+
+</script>
+
+
+<style>
+    nav {
+        width: 100%;
+        display: flex;
+    }
+    ul {
+        display: flex;
+    }
+    li {
+        list-style: none;
+    }
+    .actual button {
+        background-color: #000;
+        color: #fff;
+    }
+
+</style>
+
+
+{#if mostrar}
+    <div class="Paginacion">
+    
+        <nav>
+            <ul>
+                {#each paginas as pagina, i ("pagina_"+i)}
+
+                    <li class={ i == actual ? "actual" : "" }>
+                        <button on:click={e=>accion(e,i)}>
+                            {i}
+                        </button>
+                    </li>
+
+                {/each}
+            </ul>
+        </nav>
+    </div>
+{/if}
+
+
---
+++ src/stories/componentes/Paginacion/Paginacion.stories.js
@@ -0,0 +1,45 @@
+import Paginacion from '../../../componentes/Paginacion/Paginacion.svelte';
+
+export default {title: 'Componentes/Paginacion/Paginacion'}
+
+
+export const PaginacionDefault = () => ({
+    Component: Paginacion,
+    props: {
+        elementos: 24, // total de elementos a representar
+        elementosPagina: 4, // cuantos se muestran por pag.
+        pagina: 0, // el índice de pag actual
+        seleccionar: i => console.log(i) // función que consume el índice
+    },
+});
+
+
+
+
+export const PaginacionArreglo = () => ({
+    Component: Paginacion,
+    props: {
+        elementos: ["hola","soy","un","array"], // total de elementos a representar
+        elementosPagina: 4, // cuantos se muestran por pag.
+        pagina: 0, // el índice de pag actual
+        seleccionar: i => console.log(i) // función que consume el índice
+    },
+});
+
+
+
+// errores
+
+export const PaginacionNoMostrar = () => ({
+    Component: Paginacion,
+    props: {
+        seleccionar: i => console.log(i) // función que consume el índice
+    },
+});
+
+export const PaginacionNoMostrar2 = () => ({
+    Component: Paginacion,
+    props: {
+        elementos: 10,
+    },
+});
