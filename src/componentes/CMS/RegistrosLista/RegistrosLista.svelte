0ebd27fc3376445948117e070d23468c83bdbb5e
josecaos <jazzvoon@gmail.com>
2020-04-30 21:37:47 -0500

    Fixes a RegistroTarjeta

--- src/componentes/CMS/RegistrosLista/RegistrosLista.svelte
+++ src/componentes/CMS/RegistrosLista/RegistrosLista.svelte
@@ -1,13 +1,16 @@
 <script>
   import ListaComponentes from "../../Listados/ListaComponentes/ListaComponentes.svelte";
-
+  import RegistroTarjeta from "../RegistroTarjeta/RegistroTarjeta.svelte";
   export let elementos;
   export let mensajeVacio = "No hay Registros";
+
+  $: elementosMostrar = elementos.map(e => ({
+    componente: RegistroTarjeta,
+    data: e
+  }));
 </script>
 
-
 <style>
-
   #RegistrosLista :global(.ListaComponentes ul) {
     margin: 0;
     padding: 0;
@@ -29,6 +32,7 @@
 
 <section id="RegistrosLista">
 
-  <ListaComponentes {elementos} {mensajeVacio} />
-
+  {#if Array.isArray(elementosMostrar)}
+    <ListaComponentes elementos={elementosMostrar} {mensajeVacio} />
+  {/if}
 </section>
