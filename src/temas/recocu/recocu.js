283e6a15fb1ec628dbb90a033e8b7b407161dd49
ledautopica <vane.multimedia@gmail.com>
2020-05-14 15:58:44 -0500

    creando theaming recocu

---
+++ src/temas/recocu/recocu.js
@@ -0,0 +1,41 @@
+import variables from './variables';
+import estilos from './estilos';
+
+const tema = {
+    name: 'Recocu',
+    properties: {
+        colores: variables.colores,
+        textos: {
+            parrafo: {
+                tipografia: estilos.tipografia.parrafo,
+                color: variables.colores.texto.oscuro
+            }
+        },
+        botones: {
+            primario: {
+            fondo: variables.colores.primario.base,
+            color: variables.colores.texto.claro,
+            espacio: variables.espaciados.md,
+            tipografia: estilos.tipografia.parrafo,
+            familia: variables.tipografia.fuentes.complementaria.nombre    
+            },
+            secundario: {
+            fondo: variables.colores.primario.oscuro,
+            color: variables.colores.texto.claro,
+            espacio: variables.espaciados.sm,
+            tipografia: estilos.tipografia.parrafo,
+            familia: variables.tipografia.fuentes.complementaria.nombre
+            }          
+        },
+        espaciado: {
+            padding: variables.espaciados.md,
+        },
+    },
+    fontFamilies: {
+        principal: variables.tipografia.familias.principal,
+        secundaria: variables.tipografia.familias.secundaria,
+        complementaria: variables.tipografia.familias.complementaria
+    }
+};
+
+export default tema;
---
+++ src/temas/recocu/variables.js
@@ -0,0 +1,76 @@
+const variables = {
+    colores: {
+        texto: {
+            base: "#4C4CFF",
+            claro: '#FFF',
+            oscuro: '#282230',
+        },
+        fondo: {
+            base: "#666666",
+            claro: "#CCCCCC",
+            oscuro: "#212121",    
+        },
+        primario: {
+            base: "#F36957",
+            claro: "#FF9A84",
+            oscuro: "#BB382D",
+        },
+        secundario_a: {
+            base: "#3849C4",
+            claro: "#7374F8",
+            oscuro: "#002192",
+        },
+       
+        complementario: {
+            base: "#D9A5D6",
+            claro: "#D9A5D6",
+            oscuro: "#A776A5",
+        }
+    },
+    tipografia: {
+        familias: {
+            principal: "https://fonts.googleapis.com/css2?family=Jost&display=swap",
+            secundaria: "https://fonts.googleapis.com/css2?family=Ubuntu&display=swap",
+            complementaria:"https://fonts.googleapis.com/css2?family=Frank+Ruhl+Libre&display=swap"
+            },
+        fuentes: {
+            principal: {
+                nombre: "Jost Sans",
+                regular: 400,
+                ligero: 300,
+                negrita: 700,
+                grueso: 900
+            },
+            secundaria: {
+                nombre: "Ubuntu Sans",
+                regular: 400,
+                ligero: 300,
+                negrita: 700,
+                grueso: 900
+            },
+            complementaria: {
+                nombre: "Frank Sans",
+                regular: 400,
+                ligero: 300,
+                negrita: 700,
+                grueso: 900
+            },
+        },
+        tamannos: {
+            xs: "0.5rem",
+            sm: "0.75rem",
+            md: "1rem",
+            lg: "1.25rem",
+            xl: "1.5rem",
+        }
+    },
+    espaciados: {
+        xs: "0.25rem",
+        sm: "0.5rem",
+        md: "0.75rem",
+        lg: "1rem",
+        xl: "1.25rem",
+    }
+}
+
+export default variables
