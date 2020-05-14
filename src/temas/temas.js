171384bec486fd7889dfbc73fed70950abf274c7
josecaos <jazzvoon@gmail.com>
2020-05-14 16:13:57 -0500

    Exporta tema recocu

--- src/temas/temas.js
+++ src/temas/temas.js
@@ -1,10 +1,12 @@
 import ccd from './ccd/ccd';
 import ccd_claro from './ccd_claro/ccd_claro';
 import ccd_oscuro from './ccd_oscuro/ccd_oscuro';
+import recocu from './recocu/recocu';
 
 
 export default [
     ccd,
     ccd_claro,
     ccd_oscuro,
+    recocu,
 ]
