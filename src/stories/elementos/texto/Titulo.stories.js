c84745b5364d51f1fecc72d136faea006356bae5
ledautopica <vane.multimedia@gmail.com>
2020-05-19 21:35:36 -0500

    A

--- src/stories/elementos/texto/Titulo.stories.js
+++ src/stories/elementos/texto/Titulo.stories.js
@@ -1,68 +1,93 @@
+import ThemeTester from '../../../componentes/ThemeTester/ThemeTester.svelte';
 import Titulo from '../../../elementos/texto/Titulo/Titulo.svelte';
 
-export default { title: 'Elementos/Texto/Titulo' }
+export default { title: 'Elementos/Texto/Titulo' } 
 
 
 export const tituloDefault = () => ({
-    Component: Titulo,
+    Component: ThemeTester,
     props: {
-        texto: 'Título Default',
-        color: '#000'
+    componente: Titulo,
+            datos:{
+            texto: 'Título Default',
+            color: '#000'
+            }    
     },
 });
 
 export const tituloColor = () => ({
-    Component: Titulo,
+    Component: ThemeTester,
     props: {
+        componente: Titulo,
+        datos:{
         texto: 'Título Color',
         color: '#f0a'
+        }
     },
 });
 
 export const titulo1 = () => ({
-    Component: Titulo,
+    Component: ThemeTester,
     props: {
+        componente: Titulo,
+        datos:{
         texto: 'Título 1',
         nivel: 1
+        }
     },
 });
 
 export const titulo2 = () => ({
-    Component: Titulo,
+    Component: ThemeTester,
     props: {
+        componente: Titulo,
+        datos:{
         texto: 'Título 2',
         nivel: 2
+        }
     },
 });
 
 export const titulo3 = () => ({
-    Component: Titulo,
+    Component: ThemeTester,
     props: {
+        componente: Titulo,
+        datos:{
         texto: 'Título 3',
         nivel: 3
+        }
     },
 });
 
 export const titulo4 = () => ({
-    Component: Titulo,
+    Component: ThemeTester,
     props: {
+        componente: Titulo,
+        datos:{
         texto: 'Título 4',
         nivel: 4
+        }
     },
 });
 
 export const titulo5 = () => ({
-    Component: Titulo,
+    Component: ThemeTester,
     props: {
+        componente: Titulo,
+        datos:{
         texto: 'Título 5',
         nivel: 5
+        }
     },
 });
 
 export const titulo6 = () => ({
-    Component: Titulo,
+    Component: ThemeTester,  
     props: {
+        componente: Titulo,
+        datos:{
         texto: 'Título 6',
         nivel: 6
+        }
     },
 });
