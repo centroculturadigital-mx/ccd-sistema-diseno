a801e7a7e9e83b2e95ed0eb62c0b316b7c419a98
ledautopica <vane.multimedia@gmail.com>
2020-05-26 15:21:13 -0500

    ThemeTester MenuPasos

--- src/stories/componentes/Navegacion/pasos.menu.stories.js
+++ src/stories/componentes/Navegacion/pasos.menu.stories.js
@@ -1,12 +1,16 @@
+import ThemeTester from '../../../componentes/ThemeTester/ThemeTester.svelte';
 import PasosMenu from '../../../componentes/Navegacion/PasosMenu/PasosMenu.svelte';
 
-export default { title: 'Componentes/Navegacion/PasosMenu' }
+export default { title: 'Componentes/Navegacion/PasosMenu' } 
 
 export const pasosMenuDefault = () => ({
-    Component: PasosMenu,
+    Component: ThemeTester,
     props: {
+        componente: PasosMenu,
+        datos: {
         pasos: 3,
         activo: 2,
         accion: i => console.log("pasos",i)
+    }
     },
 });
