68d8aba6cde472e2cebe405063597101d03d4893
ledautopica <vane.multimedia@gmail.com>
2020-06-03 22:20:54 -0500

    Primeros estilos ListaNavegacion

--- src/componentes/Navegacion/NavegacionListaElemento/NavegacionListaElemento.svelte
+++ src/componentes/Navegacion/NavegacionListaElemento/NavegacionListaElemento.svelte
@@ -1,12 +1,19 @@
 <script>
   import ListaElemento from ".././../../elementos/listas/ListaElemento/ListaElemento.svelte";
 
-  export let elementos;
+  export let elementos;   
 
   // $: elementos = typeof elementos == 'object' ? elementos : {};
 
   // $: elementos = Array.isArray( elementos ) ? elementos : [];
 </script>
+
+<style>
+ul {
+    list-style: none;
+    padding: 0;
+}
+</style>
 
 <ul>
   {#if Array.isArray(elementos)}
