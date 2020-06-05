a721864b8ae4862884f7dd3cd8eaa8ed2b2ff0a1
furenku <furenku@gmail.com>
2020-06-04 19:03:43 -0500

    interaccion y nueva estructura de NavegacionLista

--- src/stories/componentes/Navegacion/NavegacionLista/navegacion.lista.stories.js
+++ src/stories/componentes/Navegacion/NavegacionLista/navegacion.lista.stories.js
@@ -4,29 +4,22 @@ import NavegacionLista from '../../../../componentes/Navegacion/NavegacionLista/
 export default { title: 'Componentes/Navegacion/NavegacionLista/NavegacionLista' }
 
 let elementos = [{
-        enlace: "http://hola.com",
         texto: "Hola"
     },
     {
-        enlace: "http://mundo.com",
         texto: "Mundo"
     },
     {
-        enlace: "http://madre.com",
         texto: "Madre",
         elementos: [{
-                enlace: "http://hija1.com",
                 texto: "Hija 1"
             },
             {
-                enlace: "http://hija2.com",
                 texto: "Hija 2",
                 elementos: [{
-                        enlace: "http://nieta1.com",
                         texto: "Nieta 1"
                     },
                     {
-                        enlace: "http://nieta2.com",
                         texto: "Nieta 2"
                     },
                 ]
@@ -44,6 +37,18 @@ export const navegacionListaDefault = () => ({
         }
     },
 });
+
+export const navegacionListaAccion = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: NavegacionLista,
+        datos: {
+            elementos,
+            accion: () => alert("una accion desde afuera!")
+        }
+    },
+});
+
 export const navegacionListaVacia = () => ({
     Component: ThemeTester,
     props: {
