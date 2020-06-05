36c12aa4b2a7fc33e19d25a7cd2413e543505a27
josecaos <jazzvoon@gmail.com>
2020-06-05 11:48:27 -0500

    Inicia Dialogo

---
+++ src/componentes/Mensajes/Dialogo/Dialogo.svelte
@@ -0,0 +1,12 @@
+<script>
+import Texto from '../../../elementos/texto/Texto/Texto.svelte';
+import Boton from '../../../elementos/botones/Boton/Boton.svelte';
+</script>
+
+
+<section class="Dialogo">
+
+<Texto texto={"Aviso de la tarjeta diálogo"}/>
+<Boton texto={"Accion 1"}/>
+<Boton texto={"Accion 2"}/>
+</section>
