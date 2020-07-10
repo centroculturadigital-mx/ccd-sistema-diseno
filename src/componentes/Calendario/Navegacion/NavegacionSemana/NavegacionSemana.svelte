b750829c6fbe383eebb9dbf4bc02be63dbadc055
josecaos <jazzvoon@gmail.com>
2020-07-10 18:01:25 -0500

    NavegacionSemanal listo

--- src/componentes/Calendario/Navegacion/NavegacionSemana/NavegacionSemana.svelte
+++ src/componentes/Calendario/Navegacion/NavegacionSemana/NavegacionSemana.svelte
@@ -3,17 +3,20 @@
 
   import moment from "moment";
 
-  export let diaActual = 0;
+  export let diaActual;
+
   export let seleccionar = i => {
-    diaActual = i;
+    diaActual = (i - 1);
   };
 
-  const dias = new Array(365).fill(true).map((e, i) => ({
-      nombre: moment().add(i++,"d").format("ddd"),
-      numero: moment().add(i-1,"d").format("D"),
-
+  const dias = new Array(366).fill(true).map((e, i) => ({
+    nombre: moment("01/01/2020")
+      .add(i, "d")
+      .format("ddd"),
+    numero: moment("01/01/2020")
+      .add(i, "d")
+      .format("D")
   }));
-
 
   const clasesCalcular = (dia, i) => (dia == i ? "activo" : "");
 
@@ -26,6 +29,7 @@
     flex-direction: row;
     list-style-type: none;
     width: 100%;
+    max-width: 20rem;
     /* overflow: hidden; */
     overflow-x: scroll; /*no definitivo*/
     padding: 0;
@@ -33,6 +37,12 @@
     box-sizing: border-box;
   }
   li {
+    padding: calc(var(--theme-espaciados-padding) / 2);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    cursor: pointer;
     box-sizing: border-box;
   }
   .NavegacionSemana :global(button) {
@@ -41,23 +51,38 @@
     background-color: transparent;
     color: var(--theme-textos-parrafo-color);
     border: 0;
-    padding: var(--theme-espaciados-padding) var(--theme-espaciados-padding);
+    padding: calc(var(--theme-espaciados-padding) /2 )  calc(var(--theme-espaciados-padding) / 2);
     box-sizing: border-box;
   }
   .NavegacionSemana :global(button:hover) {
-    opacity: 0.75;
-    background-color: rgba(170, 170, 170, 0.25);
+    opacity: 0.5;
+    background-color: rgba(120, 120, 120, 0.25);
+  }
+  .NavegacionSemana :global(button:focus),
+  .NavegacionSemana :global(button:active) {
+    outline: 0 !important;
+    background-color: transparent !important;
   }
   .activo :global(button) {
     font-weight: bold !important;
     background-color: transparent;
+    color: var(--theme-colores-primario);
+  }
+  .evento {
+    background-color: var(--theme-colores-primario);
+    border-radius: 5rem;
+    height: 0.35rem;
+    width: 0.35rem;
   }
 </style>
 
 <ul class="NavegacionSemana">
   {#each dias as dia, i (dia)}
     <li class={clasesCalcular(diaActual, i)}>
-      <Boton texto={dia.nombre + "\n" +dia.numero} click={() => seleccionar(i)} />
+      <Boton
+        texto={dia.nombre + '\n' + dia.numero}
+        click={() => seleccionar(i)} />
+      <div class="evento" />
     </li>
   {/each}
 </ul>
--- src/stories/componentes/Calendario/NavegacionSemana.stories.js
+++ src/stories/componentes/Calendario/NavegacionSemana.stories.js
@@ -1,6 +1,8 @@
 import ThemeTester from '../../../componentes/ThemeTester/ThemeTester.svelte';
 import NavegacionSemana from '../../../componentes/Calendario/Navegacion/NavegacionSemana/NavegacionSemana.svelte';
 
+import moment from 'moment';
+
 export default { title: 'Componentes/Calendario/Navegacion/Semana' }
 
 export const navegacionSemanaDefault = () => ({
@@ -15,15 +17,16 @@ export const navegacionSemanaDefault = () => ({
     },
 });
 
-// export const navegacionSemanaActual = () => ({
-//     Component: ThemeTester,
-//     props: {
-//         componente: NavegacionSemana,
-//         datos: {
-//             mesActual: 4
-//         }
-//     },
-// });
+export const navegacionSemanaActual = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: NavegacionSemana,
+        datos: {
+            diaActual: moment().dayOfYear() - 1,
+            mesActual: moment().month(),
+        }
+    },
+});
 
 // export const navegacionSemanaSeleccionar = () => ({
 //     Component: ThemeTester,
