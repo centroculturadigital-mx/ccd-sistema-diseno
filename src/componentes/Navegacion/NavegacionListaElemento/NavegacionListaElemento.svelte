06ada28c804bd34280b9e6d29ada3ecf072dca89
furenku <furenku@gmail.com>
2020-07-21 22:24:38 -0500

    probar cambio en key en elemento NavegacionLista

--- src/componentes/Navegacion/NavegacionListaElemento/NavegacionListaElemento.svelte
+++ src/componentes/Navegacion/NavegacionListaElemento/NavegacionListaElemento.svelte
@@ -87,7 +87,7 @@
 
   {#if !colapsado && Array.isArray(subElementos) && subElementos.length > 0}
     <ul transition:slide>
-      {#each subElementos as subElemento,i  ({ texto: subElemento.texto, elementos: subElemento.elementos.map(e=>e.texto), i })}
+      {#each subElementos as subElemento,i  ({ texto: subElemento.texto, elementos: Array.isArray(subElemento.elementos) && subElemento.elementos.map(e=>e.texto), i })}
         <svelte:self elemento={subElemento} {accion}/>
       {/each}
     </ul>
