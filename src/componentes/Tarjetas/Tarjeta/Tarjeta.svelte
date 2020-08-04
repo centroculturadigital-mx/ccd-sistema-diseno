e24b41eccf617219d1cf41cbc234ec3faea341be
josecaos <jazzvoon@gmail.com>
2020-08-04 14:29:54 -0500

    Tarjeta: Tipo Horizontal y vertical + historias v1

--- src/componentes/Tarjetas/Tarjeta/Tarjeta.svelte
+++ src/componentes/Tarjetas/Tarjeta/Tarjeta.svelte
@@ -1,51 +1,351 @@
 <script>
-  import TarjetaHorizontal from "../TarjetaHorizontal/TarjetaHorizontal.svelte";
-  import TarjetaVertical from "../TarjetaVertical/TarjetaVertical.svelte";
- 
+  import Imagen from "../../../elementos/media/Imagen/Imagen.svelte";
+  import Titulo from "../../../elementos/texto/Titulo/Titulo.svelte";
+  import Boton from "../../../elementos/botones/Boton/Boton.svelte";
+  import BotonSecundario from "../../../elementos/botones/BotonSecundario/BotonSecundario.svelte";
+  import Parrafo from "../../../elementos/texto/Parrafo/Parrafo.svelte";
+  import Enlace from "../../../elementos/enlaces/Enlace/Enlace.svelte";
+  import Texto from "../../../elementos/texto/Texto/Texto.svelte";
+  import Icono from "../../../elementos/Icono/Icono.svelte";
+
+  import limitaTexto from "../../../funciones/limitaTexto";
+
+  export let tipo = "Vertical"; //"Vertical", "Horizontal", "Chica"
+  export let pleca;
   export let enlace;
   export let imagen;
   export let nombre;
+  export let nivelTitulo = 3;
   export let subtitulo;
-  export let nivelTitulo;
-  export let descripcion;
+  export let extracto;
   export let sombra;
   export let leyenda;
   export let enlaces = [];
   export let acciones = [];
+  export let extractoExtension = 25;
 
-  let ventana;
-  let breakpoint = 720;
+  $: linkTarget = enlace ? (enlace.externo ? { target: "_blank" } : {}) : {};
+
+  let colorFondoPleca = !!pleca
+    ? pleca.colores
+      ? pleca.colores.fondo
+      : "#fff"
+    : "#fff";
+
+  let colorTextoPleca = !!pleca
+    ? pleca.colores
+      ? pleca.colores.texto
+      : "#000"
+    : "#000";
+
+  $: horizontal = tipo == "Horizontal" ? "TarjetaHorizontal" : "";
 </script>
 
 <style>
-
+  .Tarjeta {
+    position: relative;
+    max-width: 20rem;
+    height: auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    border-radius: var(--theme-tarjetas-esquina);
+    background-color: var(--theme-tarjetas-fondo);
+    color: var(--theme-textos-parrafo-color);
+    overflow: hidden;
+  }
+  .TarjetaHorizontal {
+    max-width: 36rem !important;
+    display: flex;
+    height: initial !important;
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+  .Imagen {
+    width: 100%;
+    height: 10rem;
+  }
+  .Contenido {
+    position: relative;
+    box-sizing: border-box;
+    width: 100%;
+  }
+  .Textos {
+    box-sizing: border-box;
+    padding: var(--theme-espaciados-padding)
+      calc(var(--theme-espaciados-padding) * 2);
+    width: 100%;
+  }
+  .Acciones {
+    display: flex;
+    justify-content: flex-end;
+    width: 100%;
+  }
+  .Acciones :global(button) {
+    border: none;
+    padding: var(--theme-espaciados-padding);
+  }
+  .Acciones :global(button:hover) {
+    border: none;
+  }
+  .Acciones :global(a) {
+    border: none;
+    padding: var(--theme-espaciados-padding);
+    background-color: var(--theme-botones-secundario-fondo);
+    color: var(--theme-botones-secundario-color);
+  }
+  .Acciones :global(a span) {
+    font-size: calc(var(--theme-textos-enlaces-tamanno) / 1.125);
+  }
+  .Acciones :global(a:hover) {
+    border: none;
+  }
+  .sombra {
+    box-shadow: -1px 2px 3px rgba(0, 0, 0, 0.1);
+  }
+  .Subtitulo :global(h4) {
+    /*margin: 0.5rem 0;*/
+    margin: var(--theme-espaciados-margen) 0;
+  }
+  .Leyenda {
+    margin: 0.5rem 0;
+    color: var(--theme-textos-parrafo-color);
+    font-weight: var(--theme-textos-parrafo-peso);
+    font-size: var(--theme-tamannos-sm);
+  }
+  .Leyenda :global(span) {
+    color: var(--theme-textos-parrafo-color);
+    font-weight: var(--theme-textos-parrafo-peso);
+    font-size: var(--theme-tamannos-sm);
+  }
+  .Enlaces {
+    display: grid;
+    grid-template-columns: repeat(2, 50%);
+  }
+  .Enlaces :global(a span) {
+    padding: calc(var(--theme-espaciados-padding) / 2);
+    padding-left: 0;
+    font-size: var(--theme-tamannos-sm);
+    font-weight: 700;
+    color: var(--theme-textos-enlaces-color);
+    height: auto;
+  }
+  hr {
+    border-color: var(--theme-bordes-neutro);
+    border-style: solid;
+    border-width: thin;
+    opacity: 0.25;
+  }
+  footer {
+    width: 100%;
+    padding: 0 1rem 0.5rem 1rem;
+    box-sizing: border-box;
+  }
+  .enlazado {
+    text-decoration: none;
+    width: 100%;
+    height: 100%;
+    display: block;
+  }
+  .Titulo :global(h3) {
+    /*margin: 0.5rem 0;*/
+    margin: var(--theme-espaciados-margen) 0;
+  }
+  .Extracto :global(p) {
+    margin: var(--theme-espaciados-margen) 0;
+    height: auto;
+    margin-bottom: 0;
+  }
+  .Pleca {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    position: absolute;
+    top: 2rem;
+    left: 0;
+    /* min-width: 35%; */
+    padding: calc(var(--theme-espaciados-padding) / 2)
+      var(--theme-espaciados-padding);
+    max-width: 66%;
+    box-sizing: border-box;
+    color: #000;
+    fill: currentColor;
+  }
+  .Pleca :global(.iconoContenedor > *) {
+    height: 1.5rem !important;
+    width: 0.75rem;
+    margin-left: 0.5rem;
+    fill: inherit;
+  }
+  .Pleca :global(span) {
+    white-space: pre-wrap;
+    text-transform: uppercase;
+    font-size: 14px;
+    letter-spacing: 5%;
+    font-weight: normal;
+    line-height: 1.5;
+    width: 100%;
+  }
+  .Pleca .Textos {
+    display: block;
+    padding: 0;
+    margin: 0;
+  }
+  .Pleca .Textos :global(span) {
+    width: auto;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+  .Pleca .Textos :global(span:last-child) {
+    margin-bottom: 0;
+  }
+  /* tipos */
 </style>
 
-<svelte:window bind:innerWidth={ventana} />
+<article class="{horizontal} Tarjeta {!!sombra ? ' sombra' : ''}">
+  <div class="{horizontal} Contenido">
+    <div class="{horizontal} Imagen">
+      {#if !!enlace}
+        <a class="{horizontal} enlazado" {...linkTarget} href={enlace}>
+          <Imagen {imagen} alt={nombre} ajuste="cover" />
+        </a>
+      {:else}
+        <Imagen {imagen} alt={nombre} ajuste="cover" />
+      {/if}
+      {#if typeof pleca == 'object'}
+        <div class="Pleca" style={`background-color: ${colorFondoPleca}`}>
+          {#if !!pleca.texto}
+            <Texto
+              texto={pleca.texto}
+              variante="COMPACTO"
+              css={{ color: colorTextoPleca }} />
+          {/if}
+          {#if Array.isArray(pleca.textos)}
+            <ul class="Textos">
+              {#each pleca.textos as texto}
+                <Texto
+                  {texto}
+                  variante="COMPACTO"
+                  css={{ color: colorTextoPleca }} />
+              {/each}
+            </ul>
+          {/if}
+          {#if !!pleca.icono}
+            <Icono icono={pleca.icono} color={colorTextoPleca} />
+          {/if}
+        </div>
+      {/if}
+    </div>
+    <div class="Textos">
+      {#if !!leyenda}
+        <div class="Leyenda">
+          <Parrafo texto={leyenda} nivel={5} />
+        </div>
+      {/if}
+      {#if !!enlace}
+        <a class="{horizontal} enlazado" {...linkTarget} href={enlace}>
+          {#if !!nombre}
+            <div class="Titulo">
+              <Titulo texto={nombre} nivel={nivelTitulo} />
+            </div>
+          {/if}
+          {#if !!subtitulo}
+            <div class="Subtitulo">
+              <Titulo texto={subtitulo} nivel={4} />
+            </div>
+          {/if}
+        </a>
+      {:else}
+        {#if !!nombre}
+          <div class="Titulo">
+            <Titulo texto={nombre} nivel={nivelTitulo} />
+          </div>
+        {/if}
+        {#if !!subtitulo}
+          <div class="Subtitulo">
+            <Titulo texto={subtitulo} nivel={4} />
+          </div>
+        {/if}
+      {/if}
 
-{#if ventana > breakpoint}
-  <TarjetaHorizontal
-    {enlace}
-    {imagen}
-    {nombre}
-    {subtitulo}
-    {nivelTitulo}
-    {descripcion}
-    {sombra}
-    {leyenda}
-    {enlaces}
-    {acciones} />
-{:else}
-  <TarjetaVertical
-    {enlace}
-    {imagen}
-    {nombre}
-    {subtitulo}
-    {nivelTitulo}
-    {descripcion}
-    {sombra}
-    {leyenda}
-    {enlaces}
-    {acciones} />
+      {#if Array.isArray(enlaces) && enlaces.length > 0}
+        <section>
+          <hr />
+          <div class="Enlaces">
+            {#each enlaces as enlace, i ('enlace_' + i)}
+              <Enlace enlace={enlace.enlace} texto={enlace.texto} />
+            {/each}
+          </div>
+        </section>
+      {/if}
 
-{/if}
+      {#if !!extracto}
+        {#if !!enlace}
+          <a class="{horizontal} enlazado" {...linkTarget} href={enlace}>
+            <div class="Extracto">
+              <Parrafo
+                texto={limitaTexto(extracto, extractoExtension, ' ...')} />
+            </div>
+          </a>
+        {:else}
+          <div class="Extracto">
+            <Parrafo texto={limitaTexto(extracto, extractoExtension, '...')} />
+          </div>
+        {/if}
+      {/if}
+
+<!-- footer -->
+      {#if tipo == 'Horizontal'}
+        {#if Array.isArray(acciones) && acciones.length > 0}
+          <footer class="{horizontal}">
+            <hr />
+            <div class="Acciones">
+              {#each acciones as accion (accion)}
+                {#if accion.enlace}
+                  <Enlace enlace={accion.enlace} texto={accion.texto} />
+                {/if}
+                {#if typeof accion.accion == 'function'}
+                  <BotonSecundario
+                    click={accion.accion}
+                    radius="15px"
+                    texto={accion.texto} />
+                {/if}
+              {/each}
+            </div>
+          </footer>
+        {/if}
+      {/if}
+
+    </div>
+
+  </div>
+<!-- footer vertical (unico duplicado) -->
+  {#if tipo == 'Vertical'}
+    {#if Array.isArray(acciones) && acciones.length > 0}
+      <footer>
+        <hr />
+        <div class="Acciones">
+          {#each acciones as accion (accion)}
+            {#if accion.enlace}
+              <Enlace enlace={accion.enlace} texto={accion.texto} />
+            {/if}
+            {#if typeof accion.accion == 'function'}
+              <BotonSecundario
+                click={accion.accion}
+                radius="15px"
+                texto={accion.texto} />
+            {/if}
+          {/each}
+        </div>
+      </footer>
+    {/if}
+  {/if}
+
+</article>
+
+<!-- {#if tipo === "Horizontal"}
+  Horizontal
+{/if} -->
+
+{#if tipo === 'Chica'}Chica{/if}
--- src/stories/componentes/Tarjetas/tarjeta.stories.js
+++ src/stories/componentes/Tarjetas/tarjeta.stories.js
@@ -1,8 +1,53 @@
 import ThemeTester from '../../../componentes/ThemeTester/ThemeTester.svelte';
 import Tarjeta from "../../../componentes/Tarjetas/Tarjeta/Tarjeta.svelte";
-import imagenEjemplo from '../../../../public/placeholder.jpg';
+
+import imagenEjemplo from '../../../data/recursos/placeholder.png';
+
+import moment from 'moment';
 
 export default { title: "Componentes/Tarjetas/Tarjeta" }
+
+let accionesFunciones = [{
+        texto: "Acción 1",
+        accion: () => console.log("Acción 1"),
+    },
+    {
+        texto: "Acción 2",
+        accion: () => confirm("Acción 2"),
+        // variante: "PELIGRO"
+    }
+];
+let accionesEnlaces = [{
+        texto: "Enlace 1",
+        enlace: "/enlace/1"
+    },
+    {
+        texto: "Enlace 2",
+        enlace: "/enlace/2"
+    }
+];
+
+let enlaces = [{
+        enlace: "#",
+        texto: "Enlace mediano"
+    },
+    {
+        enlace: "#",
+        texto: "Enlace un poco mas largo"
+    }, {
+        enlace: "#",
+        texto: "Enlace 3"
+    },
+    {
+        enlace: "#",
+        texto: "Enlace 4"
+    },
+];
+
+let pleca = {
+    texto: "En vivo ahora!",
+    icono: "play"
+};
 
 export const tarjetaDefault = () => ({
     Component: ThemeTester,
@@ -10,9 +55,94 @@ export const tarjetaDefault = () => ({
         componente: Tarjeta,
         datos: {
             imagen: imagenEjemplo,
+            enlace: {
+                url: "http://un-enlace.com",
+                externo: true
+            },
             nombre: 'Esto es un título.',
-            descripcion: 'Mei sanctus delenit denique in, dolore legere at eos, eam putant deserunt ei. Vidit adhuc eam ne, partem animal ne pro. Esse eligendi no qui, feugiat nonumes voluptatibus sit ei. Duis nihil aperiri sed ei.',
-            nivelTitulo: 'h3'
+            subtitulo: "Esto es un subtítulo",
+            extracto: 'Mei sanctus delenit denique in, dolore legere at eos, eam putant deserunt ei. Vidit adhuc eam ne, partem animal ne pro. Esse eligendi no qui, feugiat nonumes voluptatibus sit ei. Duis nihil aperiri sed ei.',
+            nivelTitulo: 'h3',
+            leyenda: "Un texto configurable",
+            sombra: true,
+            acciones: [accionesFunciones[0], accionesEnlaces[0]],
+            pleca,
+            enlaces
+        }
+    }
+});
+
+export const tarjetaExtractoCorto = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: Tarjeta,
+        datos: {
+            imagen: imagenEjemplo,
+            enlace: {
+                url: "http://un-enlace.com",
+                externo: true
+            },
+            nombre: 'Esto es un título.',
+            subtitulo: "Esto es un subtítulo",
+            extracto: 'Mei sanctus delenit denique in, dolore legere at eos, eam putant deserunt ei. Vidit adhuc eam ne, partem animal ne pro. Esse eligendi no qui, feugiat nonumes voluptatibus sit ei. Duis nihil aperiri sed ei.',
+            extractoExtension: 12,
+            nivelTitulo: 'h3',
+            leyenda: "Un texto configurable",
+            sombra: true,
+            acciones: [accionesFunciones[0], accionesEnlaces[0]],
+            pleca,
+            enlaces
+        }
+    }
+});
+
+export const tarjetaImagenTexto = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: Tarjeta,
+        datos: {
+            imagen: imagenEjemplo,
+            nombre: 'Tarjeta con una acción.',
+        }
+    }
+});
+
+export const tarjetaAccion = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: Tarjeta,
+        datos: {
+            imagen: imagenEjemplo,
+            nombre: 'Tarjeta con una acción.',
+            nivelTitulo: 'h3',
+            acciones: accionesFunciones.slice(0, 1),
+        }
+    }
+});
+
+
+export const tarjetaAcciones = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: Tarjeta,
+        datos: {
+            imagen: imagenEjemplo,
+            nombre: 'Tarjeta con varias acciones',
+            nivelTitulo: 'h3',
+            acciones: accionesFunciones,
+        }
+    }
+});
+
+export const tarjetaAccionEnlace = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: Tarjeta,
+        datos: {
+            imagen: imagenEjemplo,
+            nombre: 'Tarjeta con enlaces acción',
+            nivelTitulo: 'h3',
+            acciones: accionesEnlaces,
         }
     }
 });
@@ -24,7 +154,7 @@ export const tarjetaSombra = () => ({
         datos: {
             imagen: imagenEjemplo,
             nombre: 'Esto es un título.',
-            descripcion: 'Mei sanctus delenit denique in, dolore legere at eos, eam putant deserunt ei. Vidit adhuc eam ne, partem animal ne pro. Esse eligendi no qui, feugiat nonumes voluptatibus sit ei. Duis nihil aperiri sed ei.',
+            extracto: 'Mei sanctus delenit denique in, dolore legere at eos, eam putant deserunt ei. Vidit adhuc eam ne, partem animal ne pro. Esse eligendi no qui, feugiat nonumes voluptatibus sit ei. Duis nihil aperiri sed ei.',
             nivelTitulo: 'h3',
             sombra: true,
         }
@@ -38,13 +168,13 @@ export const tarjetaSubtitulo = () => ({
             imagen: imagenEjemplo,
             nombre: 'Esto es un título.',
             subtitulo: "Esto es un subtitulo",
-            descripcion: 'Mei sanctus delenit denique in, dolore legere at eos, eam putant deserunt ei. Vidit adhuc eam ne, partem animal ne pro. Esse eligendi no qui, feugiat nonumes voluptatibus sit ei. Duis nihil aperiri sed ei.',
+            extracto: 'Mei sanctus delenit denique in, dolore legere at eos, eam putant deserunt ei. Vidit adhuc eam ne, partem animal ne pro. Esse eligendi no qui, feugiat nonumes voluptatibus sit ei. Duis nihil aperiri sed ei.',
             nivelTitulo: 'h3',
             sombra: true,
         }
     }
 });
-export const tarjetaVerticalLeyenda = () => ({
+export const tarjetaLeyenda = () => ({
     Component: ThemeTester,
     props: {
         componente: Tarjeta,
@@ -55,6 +185,203 @@ export const tarjetaVerticalLeyenda = () => ({
             leyenda: moment().format("DD/MMMM/YYYY"),
             nivelTitulo: 'h3',
             sombra: true,
+        }
+    }
+});
+export const tarjetaConEnlace = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: Tarjeta,
+        datos: {
+            imagen: imagenEjemplo,
+            nombre: 'Esto es un título.',
+            subtitulo: "Esto es un subtitulo",
+            extracto: 'Mei sanctus delenit denique in, dolore legere at eos, eam putant deserunt ei. Vidit adhuc eam ne, partem animal ne pro. Esse eligendi no qui, feugiat nonumes voluptatibus sit ei. Duis nihil aperiri sed ei.',
+            nivelTitulo: 'h3',
+            leyenda: moment().format("DD/MMMM/YYYY"),
+            sombra: true,
+            // enlaces, 
+            enlace: "#"
+        }
+    }
+});
+export const tarjetaEnlaces = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: Tarjeta,
+        datos: {
+            imagen: imagenEjemplo,
+            nombre: 'Esto es un título.',
+            subtitulo: "Esto es un subtitulo",
+            extracto: 'Mei sanctus delenit denique in, dolore legere at eos, eam putant deserunt ei. Vidit adhuc eam ne, partem animal ne pro. Esse eligendi no qui, feugiat nonumes voluptatibus sit ei. Duis nihil aperiri sed ei.',
+            nivelTitulo: 'h3',
+            sombra: true,
+            enlaces,
+        }
+    }
+});
+export const tarjetaAccionesConEnlace = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: Tarjeta,
+        datos: {
+            imagen: imagenEjemplo,
+            nombre: 'Tarjeta con varias mas de una acción',
+            nivelTitulo: 'h3',
+            acciones: accionesFunciones,
+            enlace: "#"
+        }
+    }
+});
+
+export const tarjetaConPleca = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: Tarjeta,
+        datos: {
+            imagen: imagenEjemplo,
+            nombre: 'Tarjeta con una pleca de aviso',
+            acciones: accionesFunciones,
+            pleca: {
+                texto: "Texto pleca",
+                colores: {
+                    fondo: "#f00",
+                    texto: "#fff"
+                }
+            },
+        }
+    }
+});
+
+export const tarjetaConPlecaIcono = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: Tarjeta,
+        datos: {
+            imagen: imagenEjemplo,
+            nombre: 'Tarjeta con una pleca de aviso',
+            acciones: accionesFunciones,
+            pleca: {
+                texto: "Texto pleca",
+                icono: "mas",
+                colores: {
+                    fondo: "#f00",
+                    texto: "#fff"
+                }
+            },
+        }
+    }
+});
+
+export const tarjetaConPlecaIconoColor = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: Tarjeta,
+        datos: {
+            imagen: imagenEjemplo,
+            nombre: 'Tarjeta con una pleca de aviso',
+            acciones: accionesFunciones,
+            pleca: {
+                texto: "Texto pleca",
+                icono: "mas",
+                colores: {
+                    fondo: "#0561ad",
+                    texto: "#0bc1ed"
+                }
+            },
+        }
+    }
+});
+
+
+export const tarjetaConPlecaConTextos = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: Tarjeta,
+        datos: {
+            imagen: imagenEjemplo,
+            nombre: 'Tarjeta con una pleca de aviso',
+            acciones: accionesFunciones,
+            pleca: {
+                textos: [
+                    "Primer texto",
+                    "Segundo",
+                    "Tercero",
+                ],
+                colores: {
+                    fondo: "#f00",
+                    texto: "#fff"
+                }
+            },
+        }
+    }
+});
+export const tarjetaTipoVertical = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: Tarjeta,
+        datos: {
+            imagen: imagenEjemplo,
+            enlace: {
+                url: "http://un-enlace.com",
+                externo: true
+            },
+            nombre: 'Esto es un título.',
+            subtitulo: "Esto es un subtítulo",
+            extracto: 'Mei sanctus delenit denique in, dolore legere at eos, eam putant deserunt ei. Vidit adhuc eam ne, partem animal ne pro. Esse eligendi no qui, feugiat nonumes voluptatibus sit ei. Duis nihil aperiri sed ei.',
+            nivelTitulo: 'h3',
+            leyenda: "Un texto configurable",
+            sombra: true,
+            acciones: [accionesFunciones[0], accionesEnlaces[0]],
+            pleca,
+            enlaces,
+            tipo: "Vertical"
+        }
+    }
+});
+export const tarjetaTipoHorizontal = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: Tarjeta,
+        datos: {
+            imagen: imagenEjemplo,
+            enlace: {
+                url: "http://un-enlace.com",
+                externo: true
+            },
+            nombre: 'Esto es un título.',
+            subtitulo: "Esto es un subtítulo",
+            extracto: 'Mei sanctus delenit denique in, dolore legere at eos, eam putant deserunt ei. Vidit adhuc eam ne, partem animal ne pro. Esse eligendi no qui, feugiat nonumes voluptatibus sit ei. Duis nihil aperiri sed ei.',
+            nivelTitulo: 'h3',
+            leyenda: "Un texto configurable",
+            sombra: true,
+            acciones: [accionesFunciones[0], accionesEnlaces[0]],
+            pleca,
+            enlaces,
+            tipo: "Horizontal"
+        }
+    }
+});
+export const tarjetaTipoChica = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: Tarjeta,
+        datos: {
+            imagen: imagenEjemplo,
+            nombre: 'Tarjeta con una pleca de aviso',
+            acciones: accionesFunciones,
+            pleca: {
+                textos: [
+                    "Primer texto",
+                    "Segundo",
+                    "Tercero",
+                ],
+                colores: {
+                    fondo: "#f00",
+                    texto: "#fff"
+                }
+            },
+            tipo: "Chica"
         }
     }
 });
