1d9135b3af5cf1cab9c95eab08b75a5f7dba543b
josecaos <jazzvoon@gmail.com>
2020-08-26 16:39:54 -0500

    Calendario: estilos

--- src/componentes/Calendario/Calendario.svelte
+++ src/componentes/Calendario/Calendario.svelte
@@ -1,304 +1,281 @@
 <script>
+  import moment from "moment";
 
-import moment from "moment" 
+  import { onMount } from "svelte";
 
-import { onMount } from "svelte"
+  import Aviso from "../../elementos/texto/Aviso/Aviso";
 
-import Aviso from "../../elementos/texto/Aviso/Aviso";
+  import CalendarioAnno from "./CalendarioAnno/CalendarioAnno";
+  import CalendarioMes from "./CalendarioMes/CalendarioMes";
+  import CalendarioSemana from "./CalendarioSemana/CalendarioSemana";
+  import CalendarioDia from "./CalendarioDia/CalendarioDia";
+  import CalendarioCabecera from "./CalendarioCabecera/CalendarioCabecera";
 
-import CalendarioAnno from "./CalendarioAnno/CalendarioAnno";
-import CalendarioMes from "./CalendarioMes/CalendarioMes";
-import CalendarioSemana from "./CalendarioSemana/CalendarioSemana";
-import CalendarioDia from "./CalendarioDia/CalendarioDia";
-import CalendarioCabecera from "./CalendarioCabecera/CalendarioCabecera"
+  let vistaActual = 1;
 
-let vistaActual = 1
+  export let seleccionar;
+  export let eventos;
 
-export let seleccionar
-export let eventos
+  moment.locale("es_MX");
 
-moment.locale('es_MX')
+  const desseleccionar = () => {
+    diaActual = null;
+    semanaActual = null;
+    mesActual = null;
+    annoActual = null;
+  };
 
-const desseleccionar = () => {
-    diaActual=null
-    semanaActual=null
-    mesActual=null
-    annoActual=null
-}
+  const vistaSeleccionar = i => {
+    vistaActual = i;
+  };
 
+  let annoActual = moment().year();
+  let mesActual = moment().month();
+  let semanaActual = moment().week();
+  let diaActual = moment().date();
 
-const vistaSeleccionar = i => { 
-    vistaActual = i
-}
+  const calcularFecha = ({ anno, mes, dia }) =>
+    moment({
+      day: dia,
+      month: mes,
+      year: anno
+    });
 
-
-
-let annoActual=moment().year()
-let mesActual=moment().month()
-let semanaActual=moment().week()
-let diaActual=moment().date()
-
-
-const calcularFecha = ({
-    anno,
-    mes,
-    dia
-}) => moment({
-    day: dia,
-    month: mes,
-    year: anno
-})
-
-
-
-
-$: fecha = calcularFecha({
+  $: fecha = calcularFecha({
     anno: annoActual,
     mes: mesActual,
     dia: diaActual
-})
+  });
 
+  $: vistaMostrar = vistas[vistaActual];
 
+  // $: acciones = { ...seleccionar }
 
-
-$: vistaMostrar = vistas[vistaActual]
-
-// $: acciones = { ...seleccionar }
-
-$: vistas = [
+  $: vistas = [
     {
-        clave: "anno",
-        nombre: "Año",
-        componente: CalendarioAnno,
-        data: {
-            // accion: seleccionarAnnoActual,
-            fecha,
-            eventos
+      clave: "anno",
+      nombre: "Año",
+      componente: CalendarioAnno,
+      data: {
+        // accion: seleccionarAnnoActual,
+        fecha,
+        eventos
+      }
+    },
+    {
+      clave: "mes",
+      nombre: "Mes",
+      componente: CalendarioMes,
+      data: {
+        // accion: seleccionarMesActual,
+        fecha,
+        seleccionar: {
+          dia: i => {
+            seleccionarDiaActual(i);
+            seleccionar.dia(i);
+          },
+          mes: () => {
+            llamarAccion();
+            seleccionar.mes(fecha.set("date", 1).toDate());
+          }
         },
+        eventos
+      }
     },
     {
-        clave: "mes",
-        nombre: "Mes",
-        componente: CalendarioMes,
-        data: {
-            // accion: seleccionarMesActual,
-            fecha,
-            seleccionar: {
-                dia: (i)=>{
-                    seleccionarDiaActual(i)
-                    seleccionar.dia(i)
-                },
-                mes: ()=>{
-                    llamarAccion()
-                    seleccionar.mes(fecha.set("date",1).toDate())
-                }
-            },
-            eventos
-            
-        }
+      clave: "semana",
+      nombre: "Semana",
+      componente: CalendarioSemana,
+      data: {
+        accion: seleccionar,
+        fecha,
+        eventos
+      }
     },
     {
-        clave: "semana",
-        nombre: "Semana",
-        componente: CalendarioSemana,
-        data: {
-            accion: seleccionar,
-            fecha,
-            eventos
+      clave: "clave",
+      nombre: "Dia",
+      componente: CalendarioDia,
+      data: {
+        accion: seleccionar,
+        fecha,
+        eventos
+      }
+    }
+  ];
+
+  const anterior = () => {
+    switch (vistaMostrar.clave) {
+      case "anno":
+        annoActual--;
+        break;
+      case "mes":
+        mesActual--;
+        if (mesActual < 0) {
+          annoActual--;
+          mesActual = 11;
         }
-    },
-    {
-        clave: "clave",
-        nombre: "Dia",
-        componente: CalendarioDia,
-        data: {
-            accion: seleccionar,
-            fecha,
-            eventos
+        break;
+      case "semana":
+        semanaActual--;
+        break;
+      case "dia":
+        diaActual--;
+        if (diaActual < 0) {
+          mesActual--;
+          diaActual = moment({
+            month: mesActual,
+            year: annoActual
+          }).daysInMonth();
         }
-    },
-]
+        break;
+    }
+  };
 
+  const siguiente = () => {
+    switch (vistaMostrar.clave) {
+      case "anno":
+        annoActual++;
+        seleccionarAnnoActual(annoActual);
+        break;
+      case "mes":
+        mesActual++;
+        if (mesActual > 11) {
+          annoActual++;
+          mesActual = 0;
+        }
+        seleccionarMesActual(mesActual);
+        break;
+      case "semana":
+        semanaActual++;
+        seleccionarSemanaActual(semanaActual);
+        break;
+      case "dia":
+        diaActual++;
+        if (
+          diaActual >
+          moment({ month: mesActual, year: annoActual }).daysInMonth()
+        ) {
+          mesActual++;
+          diaActual = 0;
+        }
+        seleccionarDiaActual(diaActual);
+        break;
+    }
+  };
 
-
-
-const anterior = () => {
-    switch( vistaMostrar.clave ) {
-        case 'anno': 
-            annoActual--
-            break;
-        case 'mes': 
-            mesActual--
-            if( mesActual < 0 ) {
-                annoActual--
-                mesActual = 11
-            }
-            break;
-        case 'semana': 
-            semanaActual--
-            break;
-        case 'dia': 
-            diaActual--
-            if( diaActual < 0 ) {
-                mesActual--
-                diaActual = moment({month: mesActual, year: annoActual}).daysInMonth()
-            }   
-            break;
-    }    
-}
-
-
-
-
-
-
-const siguiente = () => {
-    switch( vistaMostrar.clave ) {
-        case 'anno': 
-            annoActual++
-            seleccionarAnnoActual(annoActual)
-            break;
-        case 'mes': 
-            mesActual++
-            if( mesActual > 11 ) {
-                annoActual++
-                mesActual = 0
-            }            
-            seleccionarMesActual(mesActual)
-            break;
-        case 'semana': 
-            semanaActual++
-            seleccionarSemanaActual(semanaActual)
-            break;
-        case 'dia': 
-            diaActual++
-            if( diaActual > moment({month: mesActual, year: annoActual}).daysInMonth() ) {
-                mesActual++
-                diaActual = 0
-            }   
-            seleccionarDiaActual(diaActual)
-            break;
-    }    
-}
-
-
-
-
-let pasos = {
+  let pasos = {
     anterior: {
-        nombre: "Anterior",
-        accion: anterior
+      nombre: "Anterior",
+      accion: anterior
     },
     siguiente: {
-        nombre: "Siguiente",
-        accion: siguiente
-    },
-
-}
-
-
-
-const llamarAccion = (inicio, final) => {
-    if( typeof seleccionar == "function" ) {
-        
-        // let inicio = {
-        //     anno: annoActual,
-        //     mes: mesActual,
-        //     dia: diaActual,
-        // }
-
-        // let final
-
-        // switch( vistaActual ) {
-        //     case 0:
-        //         final = {
-        //             anno: annoActual+1,
-        //             mes,
-        //             dia: 1
-        //         }
-        //         break;
-        //     case 1:
-
-        //         inicio.dia = 1;
-                
-        //         final = {
-        //             anno: annoActual,
-        //             mes: mesActual,
-        //             dia: moment({month:mesActual}).daysInMonth
-        //         }
-        //         break;
-        // }
-
-        final = calcularFecha(final).toDate()
-                
-        seleccionar({
-            inicio,
-            final,
-        })
+      nombre: "Siguiente",
+      accion: siguiente
     }
-}
+  };
 
-const seleccionarAnnoActual = i => {
-    
+  const llamarAccion = (inicio, final) => {
+    if (typeof seleccionar == "function") {
+      // let inicio = {
+      //     anno: annoActual,
+      //     mes: mesActual,
+      //     dia: diaActual,
+      // }
+
+      // let final
+
+      // switch( vistaActual ) {
+      //     case 0:
+      //         final = {
+      //             anno: annoActual+1,
+      //             mes,
+      //             dia: 1
+      //         }
+      //         break;
+      //     case 1:
+
+      //         inicio.dia = 1;
+
+      //         final = {
+      //             anno: annoActual,
+      //             mes: mesActual,
+      //             dia: moment({month:mesActual}).daysInMonth
+      //         }
+      //         break;
+      // }
+
+      final = calcularFecha(final).toDate();
+
+      seleccionar({
+        inicio,
+        final
+      });
+    }
+  };
+
+  const seleccionarAnnoActual = i => {
     annoActual = i;
-    llamarAccion()
-}
-const seleccionarMesActual = i => {
-    
+    llamarAccion();
+  };
+  const seleccionarMesActual = i => {
     mesActual = i;
     // llamarAccion()
-
-}
-const seleccionarSemanaActual = i => {
-    
+  };
+  const seleccionarSemanaActual = i => {
     semanaActual = i;
-    llamarAccion()
-}
-const seleccionarDiaActual = i => {
-    
+    llamarAccion();
+  };
+  const seleccionarDiaActual = i => {
     diaActual = i;
-    llamarAccion()
-}
-
-
-
-
-
+    llamarAccion();
+  };
 </script>
 
 <style>
+  .Calendario {
+    padding: calc(var(--theme-espaciados-padding) * 2);
+    /* height: 100%;
+    width: 100%; */
+    box-sizing: border-box;
+  }
+  .Calendario :global(li) {
+    cursor: pointer;
+  }
+  .Calendario :global(li:hover) {
+    opacity: 0.75;
+  }
+  ul {
+    display: flex;
+    font-family: var(--theme-textos-parrafo-tipografia);
+    box-sizing: border-box;
+  }
 
-    ul {
-        display: flex;
-        font-family: var(--theme-textos-parrafo-tipografia);
-    }
+  li {
+    list-style: none;
+    font-family: var(--theme-textos-parrafo-tipografia);
+    box-sizing: border-box;
+  }
 
-    li {
-        list-style: none;
-        font-family: var(--theme-textos-parrafo-tipografia);
-    }
-
-    button {
-        background: none;
-        border: none;
-        font-family: var(--theme-textos-parrafo-tipografia);     
-    }
-    .Vista{
-        font-family: var(--theme-textos-parrafo-tipografia);
-    }
-    h1 {
-       font-family: var(--theme-textos-parrafo-tipografia);  
-    }
-    nav {
-        font-family: var(--theme-textos-parrafo-tipografia);
-    }
-
-
+  button {
+    background: none;
+    border: none;
+    font-family: var(--theme-textos-parrafo-tipografia);
+  }
+  .Vista {
+    font-family: var(--theme-textos-parrafo-tipografia);
+  }
+  h1 {
+    font-family: var(--theme-textos-parrafo-tipografia);
+  }
+  nav {
+    font-family: var(--theme-textos-parrafo-tipografia);
+  }
 </style>
 
-<section>
+<section class="Calendario">
 
-    <!-- <nav>
+  <!-- <nav>
         <ul>
         {#each vistas as vista, i ("vista_+i")}
             <li on:click={(e)=>vistaSeleccionar(i)}>
@@ -309,10 +286,9 @@ const seleccionarDiaActual = i => {
         {/each}
         </ul>
     </nav> -->
-    {#if !! fecha && vistaMostrar }
-
-        <div class="Vista">
-            <!-- <header>
+  {#if !!fecha && vistaMostrar}
+    <div class="Vista">
+      <!-- <header>
 
             <nav>
                 <ul>
@@ -330,25 +306,19 @@ const seleccionarDiaActual = i => {
             </nav>
             </header> -->
 
-            <CalendarioCabecera 
-            titulo={fecha.format("MMMM D, YYYY")}
-            vista={vistaMostrar.nombre}
-            seleccionar={vistaMostrar.data.seleccionar[vistaMostrar.clave]}
-            rango={fecha.format("MMMM")}
-            anterior={pasos.anterior.accion}
-            siguiente={pasos.siguiente.accion}
-            />
+      <CalendarioCabecera
+        titulo={fecha.format('MMMM D, YYYY')}
+        vista={vistaMostrar.nombre}
+        seleccionar={vistaMostrar.data.seleccionar[vistaMostrar.clave]}
+        rango={fecha.format('MMMM')}
+        anterior={pasos.anterior.accion}
+        siguiente={pasos.siguiente.accion} />
 
-            <svelte:component
-                this={vistaMostrar.componente}
-                {...vistaMostrar.data}
-            />
+      <svelte:component this={vistaMostrar.componente} {...vistaMostrar.data} />
 
-        </div>
-
-    {:else}
-<!-- test -->
-        <Aviso/>
-
-    {/if}
+    </div>
+  {:else}
+    <!-- test -->
+    <Aviso />
+  {/if}
 </section>
