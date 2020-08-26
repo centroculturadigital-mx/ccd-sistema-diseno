41f256efa496882485d4651a421b575ef9998da8
josecaos <jazzvoon@gmail.com>
2020-08-26 16:40:29 -0500

    NavegacionPasos: Arregla botones y texto

--- src/componentes/Calendario/Navegacion/NavegacionPasos/NavegacionPasos.svelte
+++ src/componentes/Calendario/Navegacion/NavegacionPasos/NavegacionPasos.svelte
@@ -1,34 +1,38 @@
 <script>
-import Boton from '../../../../elementos/botones/Boton/Boton';
-import BotonIcono from '../../../../elementos/botones/BotonIcono/BotonIcono';
-import Texto from '../../../../elementos/texto/Texto/Texto';
+  import Boton from "../../../../elementos/botones/Boton/Boton";
+  import BotonIcono from "../../../../elementos/botones/BotonIcono/BotonIcono";
+  import Texto from "../../../../elementos/texto/Texto/Texto";
 
-export let rango;
-export let anterior = () => console.log("regresa");
-export let siguiente = () => console.log("avanza");
-
-
+  export let rango;
+  export let anterior = () => console.log("regresa");
+  export let siguiente = () => console.log("avanza");
 </script>
 
 <style>
-.Navegacion {
+  .Navegacion {
     display: flex;
     justify-content: space-between;
     align-items: center;
     box-sizing: border-box;
-}
-.Navegacion :global(button) {
-    padding: calc(var(--theme-botones-primario-espacio) * 1.25) calc(var(--theme-botones-primario-espacio) * 2);
+  }
+  .Navegacion :global(button) {
+    padding: calc(var(--theme-botones-primario-espacio) * 1.25)
+      calc(var(--theme-botones-primario-espacio) * 2);
     padding-left: 0;
-}
-.Navegacion :global(button:nth-child(2)) {
-    padding-right: calc(var(--theme-botones-primario-espacio) * 2);
+  }
+  .Navegacion :global(button:first-child) {
+    /* padding-right: calc(var(--theme-botones-primario-espacio) * 2); */
+    padding-right: 0;
     flex-shrink: 0;
-    box-sizing: border-box;
     background-color: transparent;
     color: var(--theme-textos-parrafo-color);
     border: 0;
-}
+    box-sizing: border-box;
+  }
+  .Navegacion :global(span) {
+    min-width: 6rem;
+    text-align: center;
+  }
 </style>
 
 <div class="Navegacion">
