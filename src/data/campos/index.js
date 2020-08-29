f8bb0a49ca74065aaf1695906da9dee0f028ff88
furenku <furenku@gmail.com>
2020-08-29 14:53:11 -0500

    agregar index para campos de stories formulario

---
+++ src/data/campos/index.js
@@ -0,0 +1,21 @@
+import campoTexto from "./texto"
+import campoNumero from "./numero"
+import campoTextarea from "./textarea"
+import campoSelector from "./selector"
+import campoEmail from "./email"
+import campoFecha from "./fecha"
+import campoContrasenna from "./contrasenna"
+import campoArchivo from "./archivo"
+import campoTelefono from "./telefono"
+
+export default [
+    campoTexto,
+    campoNumero,
+    campoTextarea,
+    campoSelector,
+    campoEmail,
+    campoFecha,
+    campoContrasenna,
+    campoArchivo,
+    campoTelefono,
+]
--- src/stories/componentes/formularios/Formulario/formulario.stories.js
+++ src/stories/componentes/formularios/Formulario/formulario.stories.js
@@ -2,6 +2,7 @@ import ThemeTester from '../../../../componentes/ThemeTester/ThemeTester.svelte'
 import FormularioTester from '../../../../componentes/formularios/Formulario/FormularioTester.svelte';
 import Formulario from '../../../../componentes/formularios/Formulario/Formulario.svelte';
 
+import campos from "../../../../data/campos/";
 import pasos from "../../../../data/pasos";
 
 export default {
@@ -27,102 +28,6 @@ const cambiar = datos => {
 }
 
 
-let campos = [
-
-    {
-        tipo: 'texto',
-        nombre: 'ccd-sd-texto',
-        requerido: true,
-        etiqueta: 'Sin caracteres especiales ',
-        valorInicial: '...',
-        validacion: (valor) => {
-            let caracteresEspeciales = new RegExp("[!@#$%^&*()+={};':|,.<>/?]"); //solo acepta guion medio y bajo
-
-            return {
-                valido: !caracteresEspeciales.test(valor),
-                error: caracteresEspeciales.test(valor) ?
-                    new Error("No cars. espec.") : null,
-                estado: caracteresEspeciales.test(valor) ?
-                    "error" : !valor ? "" : "ok"
-            }
-        }
-    },
-    {
-        tipo: 'numero',
-        nombre: 'ccd-sd-numero',
-        etiqueta: 'El numero de la bestia ',
-        valorInicial: '...',
-        validacion: (valor) => {
-
-            if (!typeof valor == "number" || valor === "") {
-                return {
-                    valido: false,
-                    estado: "error",
-                    error: new Error("Números, por favor")
-                }
-            }
-
-            if (!valor) return {
-                valido: true,
-                estado: ""
-            }
-
-            if (parseInt(valor) === 666) return {
-                valido: true,
-                estado: "ok"
-            }
-
-            return {
-                valido: false,
-                estado: "error",
-                error: new Error("Anim sint aliquip voluptate culpa excepteur aliquip.")
-            }
-        }
-    },
-    {
-        tipo: 'textarea',
-        nombre: 'ccd-sd-textarea',
-        requerido: true,
-        etiqueta: 'Menor a 10 caracteres ',
-        valorInicial: '...',
-        validacion: (valor) => {
-
-            if (!valor) return {
-                valido: true,
-                estado: ""
-            }
-
-            if (valor.length < 10) return {
-                valido: true,
-                estado: "ok"
-            }
-
-            return {
-                valido: false,
-                error: new Error("Mayor a 10 caracteres"),
-                estado: "error"
-            }
-
-        }
-    },
-    {
-        tipo: 'selector',
-        nombre: 'ccd-sd-selector',
-        requerido: true,
-        etiqueta: 'Un selector',
-        validacion: (valor) => true,
-        valor: 2,
-        opciones: [{
-                valor: 1,
-                texto: "Hola",
-            },
-            {
-                valor: 2,
-                texto: "Mundo",
-            },
-        ]
-    },
-]
 
 
 export const formularioDefault = () => ({
