e9d34a46e6a08615282d57b19fbc963b19f6d0a7
josecaos <jazzvoon@gmail.com>
2020-10-30 16:56:08 -0600

    MenuLocal: Agrega iconos en movil < 768

--- src/elementos/menu/MenuLocal/MenuLocal.svelte
+++ src/elementos/menu/MenuLocal/MenuLocal.svelte
@@ -1,8 +1,11 @@
 <script>
   import Texto from "../../texto/Texto/Texto.svelte";
+  import Icono from "../../Icono/Icono.svelte";
 
   export let elementos = [];
   export let actual = 0;
+
+  let responsivo;
 </script>
 
 <style>
@@ -22,26 +25,42 @@
     flex-wrap: wrap;
   }
   li {
-    cursor: pointer;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
     padding: var(--theme-espaciados-padding)
       calc(var(--theme-espaciados-padding) * 3);
     margin-bottom: 0.5rem;
     box-sizing: border-box;
     flex-shrink: 0;
+    cursor: pointer;
   }
   li:hover {
     opacity: 0.75;
     border-bottom: 3px solid var(--theme-pestannas-borde-hover);
+  }
+  li :global(.iconoContenedor) {
+    height: 1.25rem !important;
+    width: 1.25rem !important;
+  }
+  li :global(svg) {
+    fill: var(--theme-textos-parrafo-color) !important;
+    height: 100%;
   }
   .actual {
     border-bottom: 3px solid var(--theme-pestannas-borde-hover);
   }
 </style>
 
+<svelte:window bind:innerWidth={responsivo}/>
 <nav>
   <ul>
     {#each elementos as elemento, i ('elemento_local_' + i)}
       <li on:click={elemento.accion(i)} class={i == actual ? 'actual' : ''}>
+        {#if elemento.icono && responsivo < 768}
+          <Icono icono={elemento.icono} />
+        {/if}
         <Texto texto={elemento.texto} />
       </li>
     {/each}
--- src/stories/elementos/menu/menu.local.stories.js
+++ src/stories/elementos/menu/menu.local.stories.js
@@ -5,27 +5,28 @@ export default { title: 'Elementos/Menú/Menú Local' }
 
 let elementos = [{
         texto: "Menú 1",
+        icono: "twitter",
         accion: () => console.log("Navegación local"),
     },
     {
         texto: "Menú 2",
+        icono: "twitter",
         accion: () => console.log("Navegación local"),
-
     },
     {
         texto: "Menú 3",
+        icono: "twitter",
         accion: () => console.log("Navegación local"),
-
     },
     {
         texto: "Menú 4",
+        icono: "twitter",
         accion: () => console.log("Navegación local"),
-
     },
     {
         texto: "Menú 5",
+        icono: "twitter",
         accion: () => console.log("Navegación local"),
-
     }
 ]
 
