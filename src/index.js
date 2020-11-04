c35b2195a5833870ebaf50d835640b7724a82ab3
furenku <furenku@gmail.com>
2020-11-03 18:03:23 -0600

    agregar boton enlace a index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // Elementos
 export { default as Boton }
 from "./elementos/botones/Boton/Boton.svelte";
+export { default as BotonEnlace }
+from "./elementos/botones/BotonEnlace/BotonEnlace.svelte";
 export { default as BotonIcono }
 from "./elementos/botones/BotonIcono/BotonIcono.svelte";
 export { default as Enlace }
