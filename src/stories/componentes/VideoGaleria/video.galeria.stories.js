90715e9edade6d02ceed34530ec24303b58f7d61
furenku <furenku@gmail.com>
2021-04-14 17:13:03 -0500

    agregar imgs ejemplo para videogaleria

--- src/stories/componentes/VideoGaleria/video.galeria.stories.js
+++ src/stories/componentes/VideoGaleria/video.galeria.stories.js
@@ -1,36 +1,41 @@
 import ThemeTester from '../../../componentes/ThemeTester/ThemeTester.svelte';
 import VideoGaleria from '../../../componentes/VideoGaleria/VideoGaleria.svelte';
 
+import img01 from '../../../ejemplos/videos/01.png'
+import img02 from '../../../ejemplos/videos/02.png'
+import img03 from '../../../ejemplos/videos/03.png'
+import img04 from '../../../ejemplos/videos/04.png'
+
 const videoGaleria = {
     videos: [{
             enlace: "https://www.youtube.com/embed/U9OZFAAESRY",
             nombre: "Video 1 Convocatoria Residencias Memorial",
-            imagen: "https://www.agora-gallery.com/advice/wp-content/uploads/2015/10/image-placeholder-300x200.png"
+            imagen: img01
         },
         {
             enlace: "https://www.youtube.com/embed/OtKEAiWGi-Q",
             nombre: "Video 2 CCD - Noche Hello World",
-            imagen: "https://www.agora-gallery.com/advice/wp-content/uploads/2015/10/image-placeholder-300x200.png"
+            imagen: img02
         },
         {
             enlace: "https://www.youtube.com/embed/j5w0iT_-4cU",
             nombre: "Video 3 Fiesta de la Nube Mala",
-            imagen: "https://www.agora-gallery.com/advice/wp-content/uploads/2015/10/image-placeholder-300x200.png"
+            imagen: img03
         },
         {
             enlace: "https://www.youtube.com/embed/IF1CHAt08zg",
             nombre: "Video 4 Global Game Jam 2020 GigaSede Los Pinos",
-            imagen: "https://www.agora-gallery.com/advice/wp-content/uploads/2015/10/image-placeholder-300x200.png"
+            imagen: img04
         },
         {
             enlace: "https://www.youtube.com/embed/DfRfDhBgA5M",
             nombre: "Video 5 ¡Va el golpe!",
-            imagen: "https://www.agora-gallery.com/advice/wp-content/uploads/2015/10/image-placeholder-300x200.png"
+            imagen: img01
         },
         {
             enlace: "https://www.youtube.com/embed/TJ3DJPzP9Jw",
             nombre: "Video 6 CCD - Invitación a Inauguración: Desobediencia Electrónica",
-            imagen: "https://www.agora-gallery.com/advice/wp-content/uploads/2015/10/image-placeholder-300x200.png"
+            imagen: img02
         }
     ]
 }
