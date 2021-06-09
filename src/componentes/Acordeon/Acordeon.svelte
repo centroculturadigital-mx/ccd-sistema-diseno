9ebb3f7834346a4981b3c4e485289f53837d92eb
josecaos <jazzvoon@gmail.com>
2021-06-09 16:49:31 -0500

    Acordeon - agrega aceptar lista de componentes

--- src/componentes/Acordeon/Acordeon.svelte
+++ src/componentes/Acordeon/Acordeon.svelte
@@ -3,17 +3,39 @@
   import Titulo from "../../elementos/texto/Titulo/Titulo";
   import Contenido from "../../componentes/Contenido/Contenido";
   import Icono from "../../elementos/Icono/Icono";
+  import ListaComponentes from "../Listados/ListaComponentes/ListaComponentes";
 
   export let estado = false;
   export let titulo;
   export let nivel;
   export let contenido;
-  export let elemento;
+  export let elementos;
 
   const colapsar = () => {
     estado = !estado;
   };
+
 </script>
+
+<div class="Acordeon">
+  <header
+    on:click={Array.isArray(elementos) || !!contenido ? colapsar() : null}
+  >
+    <Titulo texto={titulo} {nivel} />
+    {#if Array.isArray(elementos) || !!contenido}
+      <Icono icono={estado ? "arriba" : "abajo"} />
+    {/if}
+  </header>
+  {#if estado}
+    <div class="contenido" transition:slide>
+      {#if Array.isArray(elementos)}
+        <ListaComponentes {elementos} />
+      {:else if !!contenido}
+        <Contenido html={contenido} />
+      {/if}
+    </div>
+  {/if}
+</div>
 
 <style>
   * {
@@ -30,12 +52,18 @@
     justify-content: space-between;
     padding: var(--theme-espaciados-padding);
   }
+  header :global(.iconoContenedor) {
+    height: 100% !important;
+    width: 1.5rem !important;
+  }
   header :global(h1),
   header :global(h2),
   header :global(h3),
   header :global(h4),
   header :global(h5),
   header :global(h6) {
+    display: flex;
+    align-items: center;
     margin: var(--theme-espaciados-margen) 0;
   }
   header:hover {
@@ -45,25 +73,5 @@
   .contenido {
     padding: var(--theme-espaciados-padding);
   }
+
 </style>
-
-<div class="Acordeon">
-  <header on:click={colapsar}>
-    <Titulo texto={titulo} {nivel} />
-    {#if !!elemento || !!contenido}
-      <Icono icono={estado ? 'arriba' : 'abajo'} />
-    {/if}
-  </header>
-  {#if estado}
-    <div class="contenido" transition:slide>
-
-      {#if !!elemento}
-        <svelte:component this={elemento.componente} {...elemento.datos} />
-      {:else if !!contenido}
-        <Contenido html={contenido} />
-      {/if}
-
-    </div>
-  {/if}
-
-</div>
