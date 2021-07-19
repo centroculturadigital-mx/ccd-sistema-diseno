af7a867d6d71a38cecc1c59713a3441d31a798ae
josecaos <jazzvoon@gmail.com>
2021-07-19 17:31:10 -0500

    GlobosInformacion listo

---
+++ src/componentes/GlobosInformacion/GlobosInformacion/GlobosInformacion.stories.js
@@ -0,0 +1,46 @@
+import ThemeTester from '../../ThemeTester/ThemeTester'
+import GlobosInformacion from './GlobosInformacion';
+
+const globos = Array(Math.ceil(Math.random()*8)).fill(true).map(g => ({
+    texto: "Lorem ipsum dolor",
+    posicion: "ARRIBA_CENTRO",
+    coordenadas: {
+        x: Math.ceil(Math.random() * 320),
+        y: Math.ceil(Math.random() * 480)
+    }
+}));
+
+export default {
+    title: 'Componentes/GlobosInformación/GlobosInformacion'
+}
+
+export const globosInformacionDefault = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: GlobosInformacion,
+        datos: {
+            globos,
+        }
+    },
+});
+export const globosInformacion = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: GlobosInformacion,
+        datos: {
+            globos,
+            estado: true
+        }
+    },
+});
+export const globosInformacionConFondo = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: GlobosInformacion,
+        datos: {
+            globos,
+            estado: true,
+            fondoMostrar: true
+        }
+    },
+});
---
+++ src/componentes/GlobosInformacion/GlobosInformacion/GlobosInformacion.svelte
@@ -0,0 +1,42 @@
+<script>
+    import GloboInformacion from "../GloboInformacion/GloboInformacion";
+
+    export let globos;
+    export let fondoMostrar;
+    export let estado;
+</script>
+
+{#if estado}
+    <section class="GlobosInformacion">
+        {#if fondoMostrar}
+            <div class="fondo" />
+        {/if}
+        {#each globos as globo, i}
+            <GloboInformacion
+            texto={globo.texto}
+            posicion={globo.posicion}
+            coordenadas={globo.coordenadas}
+            />
+        {/each}
+    </section>
+{/if}
+
+<style>
+    * {
+        box-sizing: border-box;
+    }
+    .GlobosInformacion {
+        position: relative;
+        top: 0;
+        left: 0;
+    }
+    .fondo {
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: 100vh;
+        width: 100vw;
+        background-color: rgba(0,0,0,0.35);
+        z-index: 0;
+    }
+</style>
