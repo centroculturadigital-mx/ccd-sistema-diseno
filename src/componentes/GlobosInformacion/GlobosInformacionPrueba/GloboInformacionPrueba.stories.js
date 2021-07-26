c909a8e72633b25ed83b2c493cef1c298e0073ce
josecaos <jazzvoon@gmail.com>
2021-07-26 12:10:30 -0500

    GlobosInformacionPrueba

---
+++ src/componentes/GlobosInformacion/GlobosInformacionPrueba/GloboInformacionPrueba.stories.js
@@ -0,0 +1,14 @@
+import ThemeTester from '../../ThemeTester/ThemeTester'
+import GloboInformacionPrueba from './GloboInformacionPrueba';
+
+export default { title: 'Componentes/GlobosInformación/GloboInformacionPrueba' }
+
+export const globoInformacionPruebaDefault = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: GloboInformacionPrueba,
+        datos: {
+        
+        }
+    },
+});
---
+++ src/componentes/GlobosInformacion/GlobosInformacionPrueba/GloboInformacionPrueba.svelte
@@ -0,0 +1,19 @@
+<script>
+    import GlobosInformacionContexto from "../GlobosInformacionContexto/GlobosInformacionContexto";
+
+
+</script>
+
+<section class="GloboInformacionPrueba">
+    <!-- <GlobosInformacionContexto/> -->
+</section>
+
+<style>
+    * {
+        box-sizing: border-box;
+    }
+    .GloboInformacionPrueba {
+        position: relative;
+    }
+
+</style>
