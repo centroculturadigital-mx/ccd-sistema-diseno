06bb6ccb962fb97c53df2c73d347ebcb7c03d390
josecaos <jazzvoon@gmail.com>
2021-08-02 19:14:55 -0500

    Etiqueta - Agrega icono e imagen + estilos

--- src/componentes/Etiqueta/Etiqueta.stories.js
+++ src/componentes/Etiqueta/Etiqueta.stories.js
@@ -3,7 +3,7 @@ import Etiqueta from "./Etiqueta"
 
 export default { title: 'Componentes/Etiqueta' }
 
-export const EtiquetaDefault = () => ({
+export const etiquetaDefault = () => ({
     Component: ThemeTester,
     props: {
         componente: Etiqueta,
@@ -14,13 +14,57 @@ export const EtiquetaDefault = () => ({
     },
 });
 
-export const EtiquetaSinCerrar = () => ({
+export const etiquetaSinCerrar = () => ({
     Component: ThemeTester,
     props: {
         componente: Etiqueta,
         datos: {
             texto: "Etiqueta genérica",
             cerrar: false
+        }
+    },
+});
+export const etiquetaConIcono = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: Etiqueta,
+        datos: {
+            texto: "Etiqueta genérica",
+            cerrar: false,
+            icono: "avatar"
+        }
+    },
+});
+export const etiquetaConIconoYCerrar = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: Etiqueta,
+        datos: {
+            texto: "Etiqueta genérica",
+            cerrar: true,
+            icono: "avatar"
+        }
+    },
+});
+export const etiquetaConImagen = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: Etiqueta,
+        datos: {
+            texto: "Etiqueta genérica",
+            cerrar: false,
+            imagen:"https://fakeimg.pl/350x200/?text=Diapositiva"
+        }
+    },
+});
+export const etiquetaConImagenYCerrar = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: Etiqueta,
+        datos: {
+            texto: "Etiqueta genérica",
+            cerrar: true,
+            imagen:"https://fakeimg.pl/350x200/?text=Diapositiva"
         }
     },
 });
--- src/componentes/Etiqueta/Etiqueta.svelte
+++ src/componentes/Etiqueta/Etiqueta.svelte
@@ -1,9 +1,13 @@
 <script>
     import Texto from "../../elementos/texto/Texto/Texto";
     import Icono from "../../elementos/Icono/Icono";
+    import Imagen from "../../elementos/media/Imagen/Imagen";
+    import { TextEncoder } from "util";
 
     export let texto;
     export let cerrar;
+    export let icono;
+    export let imagen;
 
     let estado = true;
 
@@ -14,6 +18,17 @@
 
 {#if estado}
     <div class="Etiqueta">
+        {#if icono || imagen}
+            <div class="media">
+                {#if icono}
+                    <Icono {icono} />
+                {/if}
+                {#if imagen}
+                    <Imagen {imagen} alt={texto} />
+                {/if}
+            </div>
+        {/if}
+
         <Texto {texto} />
 
         {#if cerrar}
@@ -33,9 +48,15 @@
         align-items: center;
         background-color: #ccc;
         border-radius: 0.25rem;
+        padding: var(--theme-espaciados-padding);
     }
     .Etiqueta :global(span) {
-        padding: calc(var(--theme-espaciados-padding) / 2);
+        padding: 0 var(--theme-espaciados-padding);
+        font-size: 0.875rem;
+    }
+    .Etiqueta :global(.iconoContenedor svg) {
+        fill: var(--theme-textos-parrafo-color);
+        height: 100%;
     }
     .cerrar {
         display: flex;
@@ -44,16 +65,24 @@
         width: auto;
         cursor: pointer;
     }
-    .cerrar:hover {
-        opacity: 0.75;
+    .media {
+        padding: 0 calc(var(--theme-espaciados-padding) / 2);
+        height: 1.5rem;
+        width: 1.5rem;
+    }
+    .media :global(.iconoContenedor) {
+        height: 100% !important;
+        width: 100% !important;
+    }
+    .cerrar:hover :global(.iconoContenedor svg) {
+        transition: 0.25s;
+        fill: #F2F2F2;
+        background-color: #666;
+        border-radius: 0.125rem;
     }
     .cerrar :global(.iconoContenedor) {
         height: 1rem !important;
         width: 1rem !important;
-        padding: calc(var(--theme-espaciados-padding) / 2);
     }
-    .cerrar :global(.iconoContenedor svg) {
-        fill: var(--theme-textos-parrafo-color);
-        height: 100%;
-    }
+
 </style>
