609dc50ecfbf7717c78e21e12a88b7d068245943
josecaos <jazzvoon@gmail.com>
2021-08-05 17:02:04 -0500

    Imagen - refactor primera pasada

--- src/elementos/media/Imagen/Imagen.svelte
+++ src/elementos/media/Imagen/Imagen.svelte
@@ -1,18 +1,87 @@
 <script>
+  import Icono from "../../Icono/Icono";
+  import Texto from "../../texto/Texto/Texto";
+  import Parrafo from "../../texto/Parrafo/Parrafo";
+import { xlink_attr } from "svelte/internal";
+
   export let imagen;
+  export let tamannos;
+  export let maximizar;
+  export let nombre;
+  export let descripcion;
+  export let descripcionPosicion;
   export let alt;
 
   export let estilos = {
     ajuste: "",
     alineacion: "",
-    altura: ""
+    altura: "",
   };
+
+  let cargando = true;
 
   $: clases = estilos.altura ? "width-auto" : "";
 
+  //
+  const cargar = () => {
+    cargando = false;
+    console.log("ya cargue");
+  };
 </script>
 
+<div class="imagen">
+  {#if !!imagen}
+    <img
+      on:click
+      on:load={cargar}
+      class={clases}
+      src={imagen}
+      {alt}
+      style="object-fit:{estilos.ajuste};object-position:{estilos.alineacion};height:{estilos.altura}"
+    />
+  {:else if !!tamannos && typeof tamannos === "object"}
+    {#if cargando}
+      <picture on:load={cargar}>
+        <!-- tamanno xl -->
+        <source media="(min-width:1680px)" srcset="{tamannos.xl.x1} 1x, {tamannos.xl.x2} 2x, {tamannos.xl.x3} 3x" />
+        <!-- tamanno lg -->
+        <source media="(min-width:1220px)" srcset="{tamannos.lg.x1} 1x, {tamannos.lg.x2} 2x, {tamannos.lg.x3} 3x" />
+        <!-- tamanno md -->
+        <source media="(min-width:768px)" srcset="{tamannos.md.x1} 1x, {tamannos.md.x2} 2x, {tamannos.md.x3} 3x" />
+        <!-- tamanno sm -->
+        <source media="(min-width:465px)" srcset="{tamannos.sm.x1} 1x, {tamannos.sm.x2} 2x, {tamannos.sm.x3} 3x" />
+        <!-- tamanno xs -->
+        <img srcset="{tamannos.xs.x1} 1x, {tamannos.xs.x2} 2x, {tamannos.xs.x3} 3x" {alt} />
+      </picture>
+    {:else}
+      <div class="cargando">
+        <img src={tamannos.cargando} {alt} />
+      </div>
+    {/if}
+  {/if}
+  {#if nombre || descripcion}
+    <div class="descripcion {descripcionPosicion ? "afuera" : ""}">
+      <Texto texto={nombre} />
+      <Parrafo texto={descripcion} />
+    </div>
+  {/if}
+  {#if maximizar}
+    <div class="maximizar" on:click={maximizar}>
+      <Icono icono={"maximizar"}/>
+    </div>
+  {/if}
+</div>
+
 <style>
+  * {
+    box-sizing: border-box;
+  }
+  .imagen {
+    position: relative;
+    height: 100%;
+    width: 100%;
+    z-index: 0;
+  }
   img {
     object-fit: cover;
     object-position: center;
@@ -22,11 +91,78 @@
   .width-auto {
     width: auto;
   }
+  .cargando {
+    filter: blur(3px);
+  }
+  .descripcion {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    padding: calc(var(--theme-espaciados-padding) * 2);
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    /* height: 50%; */
+    width: 100%;
+    /* gradiente */
+    background: transparent;
+    background: -moz-linear-gradient(
+      0deg,
+      rgba(0, 0, 0, 1) 0.5%,
+      rgba(0, 0, 0, 0) 100%
+    );
+    background: -webkit-linear-gradient(
+      0deg,
+      rgba(0, 0, 0, 1) 0.5%,
+      rgba(0, 0, 0, 0) 100%
+    );
+    background: linear-gradient(
+      0deg,
+      rgba(0, 0, 0, 1) 0.5%,
+      rgba(0, 0, 0, 0) 100%
+    );
+    z-index: 1;
+  }
+  .descripcion :global(span) {
+    color: #FFF;
+    font-weight:700;
+    font-size: 0.75rem;
+  }
+  .descripcion :global(p) {
+    margin: 0.5rem 0 0;
+  }
+  .descripcion :global(p span) {
+    font-weight:400;
+    font-size: 0.875rem;
+  }
+  .descripcion.afuera {
+    position: relative;
+    height: auto;
+    width: 100%;
+    background: transparent;
+    padding: 0.25rem 0;
+  }
+  .descripcion.afuera :global(span) {
+    color: var(--theme-textos-parrafo-color);
+  }
+  .maximizar {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    padding: calc(var(--theme-espaciados-padding) * 2);
+    z-index: 2;
+    cursor: pointer;
+  }
+  .maximizar:hover {
+    opacity: 0.75;
+  }
+  .maximizar :global(.iconoContenedor) {
+    height: 1.25rem !important;
+    width: 1.25rem !important;
+  }
+  .maximizar :global(svg) {
+    fill: #FFF;
+    height: 100%;
+  }
+  
 </style>
-
-<img
-  on:click
-  class={clases}
-  src={imagen}
-  alt={alt}
-  style="object-fit:{estilos.ajuste};object-position:{estilos.alineacion};height:{estilos.altura}" />
--- src/stories/elementos/media/imagen.stories.js
+++ src/stories/elementos/media/imagen.stories.js
@@ -2,23 +2,57 @@ import ThemeTester from '../../../componentes/ThemeTester/ThemeTester.svelte';
 import Imagen from '../../../elementos/media/Imagen/Imagen.svelte';
 import ImagenArchivo from '../../../../public/placeholder.jpg';
 
-export default { title: "Elementos/Media/Imagen" }
+let tamannos = {
+        cargando: "https://fakeimg.pl/320x320/?text=Cargando",
+        xl: {
+            x1: "https://fakeimg.pl/1680x1680/ff0000/?text=1x",
+            x2: "https://fakeimg.pl/1680x1680/ff0000/?text=2x",
+            x3: "https://fakeimg.pl/1680x1680/ff0000/?text=3x",
+        },
+        lg: {
+            x1: "https://fakeimg.pl/1220x1220/00ff00/?text=1x",
+            x2: "https://fakeimg.pl/1220x1220/00ff00/?text=2x",
+            x3: "https://fakeimg.pl/1220x1220/00ff00/?text=3x",
+        },
+        md: {
+            x1: "https://fakeimg.pl/768x768/0000ff/?text=1x",
+            x2: "https://fakeimg.pl/768x768/0000ff/?text=2x",
+            x3: "https://fakeimg.pl/768x768/0000ff/?text=3x",
+        },
+        sm: {
+            x1: "https://fakeimg.pl/465x465/0f0f0f/?text=1x",
+            x2: "https://fakeimg.pl/465x465/0f0f0f/?text=2x",
+            x3: "https://fakeimg.pl/465x465/0f0f0f/?text=3x",
+        },
+        xs: {
+            x1: "https://fakeimg.pl/320x320/f0f0ff/?text=1x",
+            x2: "https://fakeimg.pl/465x465/0f0f0f/?text=2x",
+            x3: "https://fakeimg.pl/465x465/0f0f0f/?text=3x",
+        }
+    }
+
+    export default {
+        title: "Elementos/Media/Imagen"
+    }
 
 export const imagenDefault = () => ({
     Component: ThemeTester,
     props: {
         componente: Imagen,
         datos: {
-            imagen: ImagenArchivo
+            imagen: ImagenArchivo,
+            maximizar: () => console.log("Maximizado"),
         }
     },
 });
+
 export const imagenAltTexto = () => ({
     Component: ThemeTester,
     props: {
         componente: Imagen,
         datos: {
-            alt: "Aquí va una imágen"
+            alt: "Aquí va una imágen",
+            maximizar: () => console.log("Maximizado"),
         },
     }
 });
@@ -32,7 +66,8 @@ export const imagenAjuste = () => ({
             estilos: {
                 ajuste: 'cover',
                 altura: '200px',
-            }
+            },
+            maximizar: () => console.log("Maximizado"),
         },
     }
 });
@@ -46,7 +81,56 @@ export const imagenAltura = () => ({
             estilos: {
                 ajuste: 'fill',
                 altura: '320px',
-            }
+            },
+            maximizar: () => console.log("Maximizado"),
         }
     }
+});
+export const imagenTamannosResponsivos = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: Imagen,
+        datos: {
+            tamannos,
+            maximizar: () => console.log("Maximizado"),
+        }
+    },
+});
+export const imagenConDescripcion = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: Imagen,
+        datos: {
+            tamannos,
+            nombre: "Lorem Ipsum Dolor",
+            descripcion: "Lorem Ipsum Dolor sit amet tiniebla sonorum inferno",
+            maximizar: () => console.log("Maximizado"),
+        }
+    },
+});
+export const imagenConDescripcionAfuera = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: Imagen,
+        datos: {
+            tamannos,
+            nombre: "Lorem Ipsum Dolor",
+            descripcion: "Lorem Ipsum Dolor sit amet tiniebla sonorum inferno",
+            descripcionPosicion: true,
+            maximizar: () => console.log("Maximizado"),
+        }
+    },
+});
+export const imagenMaximizar = () => ({
+    Component: ThemeTester,
+    props: {
+        componente: Imagen,
+        datos: {
+            tamannos,
+            nombre: "Lorem Ipsum Dolor",
+            descripcion: "Lorem Ipsum Dolor sit amet tiniebla sonorum inferno",
+            descripcionPosicion: false,
+            maximizar: () => console.log("Maximizado"),
+        }
+    },
 });
