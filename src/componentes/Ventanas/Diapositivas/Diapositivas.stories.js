4cfbacd956ecf935e46605595b8964f2e0bb40b7
josecaos <jazzvoon@gmail.com>
2021-08-06 12:34:44 -0500

    Diapositivas - Fix abrir cerrar

--- src/componentes/Ventanas/Diapositivas/Diapositivas.stories.js
+++ src/componentes/Ventanas/Diapositivas/Diapositivas.stories.js
@@ -1,20 +1,55 @@
 import ThemeTester from '../../ThemeTester/ThemeTester.svelte';
 import Diapositivas from "./Diapositivas"
 
+let tamannos = {
+    cargando: "https://fakeimg.pl/320x320/?text=Cargando",
+    xl: {
+        x1: "https://fakeimg.pl/1680x1680/ff0000/?text=1x",
+        x2: "https://fakeimg.pl/1680x1680/ff0000/?text=2x",
+        x3: "https://fakeimg.pl/1680x1680/ff0000/?text=3x",
+    },
+    lg: {
+        x1: "https://fakeimg.pl/1220x1220/00ff00/?text=1x",
+        x2: "https://fakeimg.pl/1220x1220/00ff00/?text=2x",
+        x3: "https://fakeimg.pl/1220x1220/00ff00/?text=3x",
+    },
+    md: {
+        x1: "https://fakeimg.pl/768x768/0000ff/?text=1x",
+        x2: "https://fakeimg.pl/768x768/0000ff/?text=2x",
+        x3: "https://fakeimg.pl/768x768/0000ff/?text=3x",
+    },
+    sm: {
+        x1: "https://fakeimg.pl/465x465/0f0f0f/?text=1x",
+        x2: "https://fakeimg.pl/465x465/0f0f0f/?text=2x",
+        x3: "https://fakeimg.pl/465x465/0f0f0f/?text=3x",
+    },
+    xs: {
+        x1: "https://fakeimg.pl/320x320/f0f0ff/?text=1x",
+        x2: "https://fakeimg.pl/465x465/0f0f0f/?text=2x",
+        x3: "https://fakeimg.pl/465x465/0f0f0f/?text=3x",
+    }
+}
 
-let diapositivas =  [{
-        imagen: "https://fakeimg.pl/350x200/?text=Diapositiva",
-        alt: "Una imagen",
+let diapositivas = [{
+    // imagen: "https://fakeimg.pl/350x200/?text=Diapositiva",
+    tamannos,
+    nombre: "Lorem Ipsum Dolor",
+    descripcion: "Lorem Ipsum Dolor sit amet tiniebla sonorum inferno",
+    alt: "Una imagen",
 }]
 
-export default { title: 'Componentes/Ventanas/Diapositivas' }
+export default {
+    title: 'Componentes/Ventanas/Diapositivas'
+}
 
 export const diapositivasDefault = () => ({
     Component: ThemeTester,
     props: {
         componente: Diapositivas,
         datos: {
-            diapositivas
+            diapositivas,
+            estado: true,
+            // cerrar: () => console.log("cerrar desde SB")
         }
     },
 });
--- src/componentes/Ventanas/Diapositivas/Diapositivas.svelte
+++ src/componentes/Ventanas/Diapositivas/Diapositivas.svelte
@@ -1,4 +1,7 @@
 <script>
+    import { fade } from "svelte/transition";
+    import Icono from "../../../elementos/Icono/Icono";
+    import Modal from "../Modal/Modal.svelte";
     import Carrusel from "../../Carrusel/Carrusel";
     import Texto from "../../../elementos/texto/Texto/Texto";
     import Titulo from "../../../elementos/texto/Titulo/Titulo";
@@ -6,33 +9,52 @@
 
     export let diapositivas;
     export let fondo;
+    export let estado;
+    export let cerrar;
 
-    $: elementosPreparados = diapositivas ? diapositivas.map((d) => ({
-        componente: Imagen,
-        data: {//TODO: Cambiar a datos desde Imagen en v1
-            ...d,
-        },
-    })) : [];
+    const cerrarDiapositivas = () => {
+        estado = !estado;
+        if (typeof cerrar == "function") {
+            cerrar();
+        }
+        console.log("Diapositivas cerrar", estado);
+    };
 
-    $: console.log("Diapositivas", diapositivas, elementosPreparados);
+    $: elementosPreparados = diapositivas
+        ? diapositivas.map((d) => ({
+              componente: Imagen,
+              data: {
+                  //TODO: Cambiar a datos desde Imagen en v1
+                  ...d,
+              },
+          }))
+        : [];
 </script>
 
-<section class="Diapositivas">
-    <!-- {#if fondo}
-        <div class="fondo" />
-    {/if} -->
+{#if !!estado}
+    <section class="Diapositivas" transition:fade|local>
+        <div class="capaCerrar" on:click={cerrarDiapositivas} />
+        <!-- {#if fondo}
+            <div class="fondo" />
+        {/if} -->
+        <header>
+            <div on:click={cerrarDiapositivas}>
+                <Icono icono={"cerrar"} />
+            </div>
+        </header>
 
-    <div class="contenido">
-        <Carrusel elementos={elementosPreparados} />
-    </div>
-</section>
+        <div class="contenido">
+            <Carrusel elementos={elementosPreparados} />
+        </div>
+    </section>
+{/if}
 
 <style>
     * {
         box-sizing: border-box;
     }
     .Diapositivas {
-        position: absolute;
+        position: fixed;
         top: 0;
         left: 0;
         background-color: rgba(0, 0, 0, 0.5);
@@ -43,18 +65,54 @@
         width: 100%;
     }
     .contenido {
-        max-width: 40rem;
-        min-width: 30rem;
+        position: relative;
+        overflow: hidden;
+        z-index: 1001;
+        /* max-width: 40rem;
+        min-width: 30rem; */
+        width: 100%;
     }
+
     .contenido :global(.Carrusel) {
         width: 100% !important;
     }
-    .fondo {
+    /* .fondo {
         position: absolute;
         top: 0;
         left: 0;
         background-color: rgba(0, 0, 0, 0.5);
         height: 100%;
         width: 100%;
+    } */
+    header {
+        position: absolute;
+        top: 0;
+        right: 0;
+        padding: calc(var(--theme-espaciados-padding) * 1);
+        height: auto;
+        width: auto;
+        display: flex;
+        justify-content: flex-end;
+        z-index: 1001;
+    }
+    header :global(img) {
+        transition: var(--theme-transisiones-normal);
+        height: 1.25rem !important  ;
+    }
+    header :global(img:hover) {
+        filter: invert();
+    }
+    header :global(.cerrar) {
+        fill: #aaaaaa !important;
+        cursor: pointer;
+    }
+    header :global(.cerrar:hover) {
+        opacity: 0.75;
+    }
+    .capaCerrar {
+        position: absolute;
+        height: 100%;
+        width: 100%;
+        z-index: 0;
     }
 </style>
