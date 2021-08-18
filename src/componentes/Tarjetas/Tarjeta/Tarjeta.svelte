10bbf1aa0fe7129d5872998fd430239252101487
furenku <furenku@gmail.com>
2021-08-18 13:00:38 -0500

    agregar enlace con nueva pestaña a Tarjeta

--- src/componentes/Tarjetas/Tarjeta/Tarjeta.svelte
+++ src/componentes/Tarjetas/Tarjeta/Tarjeta.svelte
@@ -239,7 +239,7 @@
         <div class="Acciones">
           {#each acciones as accion (accion)}
             {#if accion.enlace}
-              <Enlace enlace={accion.enlace} texto={accion.texto} />
+              <Enlace enlace={accion.enlace} texto={accion.texto} nuevaPestanna={accion.nuevaPestanna}  />
             {/if}
             {#if typeof accion.accion == "function"}
             <!-- TODO: actualizar a Boton -->
