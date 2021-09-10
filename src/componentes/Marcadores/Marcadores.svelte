3f4e3811171cdfbd2777f085e737811df55792d2
furenku <furenku@gmail.com>
2021-09-09 23:37:35 -0500

    agregar componente Marcadores y Marcador

---
+++ src/componentes/Marcadores/Marcadores.svelte
@@ -0,0 +1,32 @@
+<script>
+    
+    import Marcador from "./Marcador/Marcador"
+    
+    export let marcadores
+
+    $: marcadoresMostrar = Array.isArray( marcadores ) ? marcadores : []
+
+</script>
+
+
+<div class="Marcadores">
+
+    {#each marcadoresMostrar as marcador (marcador) }
+        {#if marcador.contenido }
+            <Marcador {...marcador}/>
+        {/if}
+    {/each}
+
+</div>
+
+<style>
+
+    .Marcadores {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+
+</style>
